896bf452189ab94bc72bc1a07747a8476f2d3248
Zeng Jie <zengjie@gmail.com>
2018-06-25 21:42:10 +0800

    Modify goto/continue to follow the chained transpilers design

--- src/targets/Transpiler.51.ts
+++ src/targets/Transpiler.51.ts
@@ -4,28 +4,5 @@ import { TSHelper as tsHelper } from "../TSHelper";
 import * as ts from "typescript";
 
 export class LuaTranspiler51 extends LuaTranspiler {
-    public transpileLoopBody(
-        node: ts.WhileStatement
-            | ts.DoStatement
-            | ts.ForStatement
-            | ts.ForOfStatement
-            | ts.ForInStatement
-    ): string {
-        this.loopStack.push(this.genVarCounter);
-        this.genVarCounter++;
-        let result = this.indent + "do\n";
-        this.pushIndent();
-        result += this.transpileStatement(node.statement);
-        this.popIndent();
-        result += this.indent + "end\n";
-        return result;
-    }
 
-    public transpileContinue(): string {
-      throw new TranspileError(
-        "Unsupported continue statement, " +
-        "continue is not supported in Lua ${this.options.luaTarget}.",
-        null
-      );
-    }
 }

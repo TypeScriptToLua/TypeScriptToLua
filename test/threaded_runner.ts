222a12e39c0f82e12444211e61d640c0ee72f5bc
Lolleko <larry.junglas@hotmail.de>
2018-07-15 21:43:17 +0200

    Fixed Typo and some formatting

--- test/threaded_runner.ts
+++ test/threaded_runner.ts
@@ -1,53 +1,56 @@
-import { MatchError } from "alsatian";
-
+import {MatchError} from "alsatian";
 import * as glob from "glob";
 import * as os from "os";
 import * as path from "path";
-
-import { Pool, config } from "threads";
+import {config, Pool} from "threads";
 
 config.set({
-    basepath : {
-        node : __dirname,
-    }
+  basepath: {
+    node: __dirname,
+  }
 });
 
 let cpuCount = os.cpus().length + 1;
 if ("TRAVIS" in process.env && "CI" in process.env) {
-    // fixed thread coutn for CI
-    cpuCount = 8;
+  // fixed thread count for CI
+  cpuCount = 8;
 }
 const testFiles: string[] = glob.sync("./test/**/*.spec.ts");
 const pool = new Pool(cpuCount);
 let jobCounter = 0;
 
-const fileArrToString = (fileArr: string[]) => fileArr.map(val => path.basename(val).replace(".spec.ts", "")).join(", ");
+const fileArrToString = (fileArr: string[]) =>
+    fileArr.map(val => path.basename(val).replace(".spec.ts", "")).join(", ");
 
-console.log (`Running tests: ${fileArrToString(testFiles)} with ${cpuCount} threads`);
+console.log(
+    `Running tests: ${fileArrToString(testFiles)} with ${cpuCount} threads`);
 
-let filesPerThread = Math.floor(testFiles.length / cpuCount);
-let threadsWithMoreWork = testFiles.length % cpuCount;
+const filesPerThread = Math.floor(testFiles.length / cpuCount);
+const threadsWithMoreWork = testFiles.length % cpuCount;
 
 for (let i = 1; i <= cpuCount; i++) {
-    let files: string[] = [];
-    if (i <= threadsWithMoreWork) {
-        files = testFiles.splice(0, filesPerThread + 1);
-    } else {
-        files = testFiles.splice(0, filesPerThread);
-    }
-    console.log(`Running tests: ${fileArrToString(files)} in thread ${i}`);
+  let files: string[] = [];
+  if (i <= threadsWithMoreWork) {
+    files = testFiles.splice(0, filesPerThread + 1);
+  } else {
+    files = testFiles.splice(0, filesPerThread);
+  }
+  console.log(`Running tests: ${fileArrToString(files)} in thread ${i}`);
 
-    pool.run("./test_thread").send({files: files})
-    .on("done", (results, input) => {
-        jobCounter++;
-        console.log(`Tests ${fileArrToString(files)} ${jobCounter}/${cpuCount} done.`);
-    })
-    .on("error", (error) => {
+  pool.run("./test_thread")
+      .send({files: files})
+      .on("done",
+          (results, input) => {
+            jobCounter++;
+            console.log(`Tests ${fileArrToString(files)} ${jobCounter}/${
+                cpuCount} done.`);
+          })
+      .on("error", error => {
         console.log("Exception in test:", files, error);
-    });
+      });
 }
 
-pool.on('finished', function() {
-        console.log('Everything done, shutting down the thread pool.');
-        pool.killAll();
-    });
+pool.on("finished", () => {
+  console.log("Everything done, shutting down the thread pool.");
+  pool.killAll();
+});

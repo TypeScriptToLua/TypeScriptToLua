bc073bb9d0144e1425fdc2bf5dcffca967861fc0
Janne Varjo <accounts@janne252.com>
2018-07-28 14:43:51 +0300

    Fix variable destructuring compatibility issue with lua 5.1 (#158)
    * Fix variable destructuring compatibility issue for lua 5.1
    
    * Remove deprecated assignmentDestructuring test files in favor of version-specific tests
    
    * fix Lua 5.3 VM compatibility issue with running Lua 5.1 code
    
    * Removed duplicate tests, renamed tests to match the test case
    
    * Fix style & coding conventions to match the project
    
    * Remove unnecessary backport
    (generated lualib no longer contains varible destruturing)

---
+++ test/unit/assignmentDestructuring.spec.ts
@@ -0,0 +1,30 @@
+import { Expect, Test, TestCase } from "alsatian";
+import { LuaTarget } from "../../src/Transpiler";
+import * as util from "../src/util";
+
+export class AssignmentDestructuringTests {
+
+    private readonly assignmentDestruturingTs = `
+        declare function myFunc(): [number, string];
+        let [a, b] = myFunc();`;
+
+    @Test("Assignment destructuring [5.1]")
+    public assignmentDestructuring51() {
+        // Transpile
+        const lua = util.transpileString(
+            this.assignmentDestruturingTs, {luaTarget: LuaTarget.Lua51, luaLibImport: "none"}
+        );
+        // Assert
+        Expect(lua).toBe(`local a,b=unpack(myFunc())`);
+    }
+
+    @Test("Assignment destructuring [5.2]")
+    public tupleDestructing52() {
+        // Transpile
+        const lua = util.transpileString(
+            this.assignmentDestruturingTs, {luaTarget: LuaTarget.Lua52, luaLibImport: "none"}
+        );
+        // Assert
+        Expect(lua).toBe(`local a,b=table.unpack(myFunc())`);
+    }
+}

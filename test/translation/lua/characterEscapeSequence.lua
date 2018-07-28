6608764bc53fa10e22d9c1924e521387732be66f
Janne <accounts@janne252.com>
2018-07-28 19:08:29 +0300

    Complex template string escape test

--- test/translation/lua/characterEscapeSequence.lua
+++ test/translation/lua/characterEscapeSequence.lua
@@ -14,3 +14,5 @@ local escapedCharsInDoubleQUotes = "\\ \0 \b \t \n \v \f \" \' \`"
 
 local escapedCharsInTemplateString = "\\ \0 \b \t \n \v \f \" \' \`"
 
+local nonEmptyTemplateString = "Level 0: \n\t "..tostring("Level 1: \n\t\t "..tostring("Level 3: \n\t\t\t "..tostring("Last level \n --").." \n --").." \n --").." \n --"
+
--- test/translation/ts/characterEscapeSequence.ts
+++ test/translation/ts/characterEscapeSequence.ts
@@ -8,3 +8,5 @@ let doubleQuoteInTemplateString = `" " "`;
 let escapedCharsInQuotes = '\\ \0 \b \t \n \v \f \" \' \`';
 let escapedCharsInDoubleQUotes = "\\ \0 \b \t \n \v \f \" \' \`";
 let escapedCharsInTemplateString = `\\ \0 \b \t \n \v \f \" \' \``;
+
+let nonEmptyTemplateString = `Level 0: \n\t ${`Level 1: \n\t\t ${`Level 3: \n\t\t\t ${'Last level \n --'} \n --`} \n --`} \n --`;

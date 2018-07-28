41847b57e8c821519694c688917acab6b19aec04
Janne <accounts@janne252.com>
2018-07-28 18:24:31 +0300

    Add test files for character escape sequences

---
+++ test/translation/lua/characterEscapeSequence.lua
@@ -0,0 +1,16 @@
+local quoteInDoubleQuotes = "\' \' \'"
+
+local quoteInTemplateString = "\' \' \'"
+
+local doubleQuoteInQuotes = "\" \" \""
+
+local doubleQuoteInDoubleQuotes = "\" \" \""
+
+local doubleQuoteInTemplateString = "\" \" \""
+
+local escapedCharsInQuotes = "\\ \0 \b \t \n \v \f \" \' \`"
+
+local escapedCharsInDoubleQUotes = "\\ \0 \b \t \n \v \f \" \' \`"
+
+local escapedCharsInTemplateString = "\\ \0 \b \t \n \v \f \" \' \`"
+
---
+++ test/translation/ts/characterEscapeSequence.ts
@@ -0,0 +1,10 @@
+let quoteInDoubleQuotes = "' ' '";
+let quoteInTemplateString = `' ' '`;
+
+let doubleQuoteInQuotes = '" " "';
+let doubleQuoteInDoubleQuotes = "\" \" \"";
+let doubleQuoteInTemplateString = `" " "`;
+
+let escapedCharsInQuotes = '\\ \0 \b \t \n \v \f \" \' \`';
+let escapedCharsInDoubleQUotes = "\\ \0 \b \t \n \v \f \" \' \`";
+let escapedCharsInTemplateString = `\\ \0 \b \t \n \v \f \" \' \``;

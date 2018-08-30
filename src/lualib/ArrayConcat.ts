8dffc3ea972c117beb99315da0a52affcb16f3b0
lolleko <lorenz.junglas@student.kit.edu>
2018-08-30 12:53:44 +0200

    Added Array.concat test

--- src/lualib/ArrayConcat.ts
+++ src/lualib/ArrayConcat.ts
@@ -1,10 +1,24 @@
-function __TS__ArrayConcat<T>(arr1: T[], arr2: T[]): T[] {
-  const out: T[] = [];
+/* tslint:disable */
+declare function pcall(func: Function): any;
+declare function type(val: any): string;
+
+function __TS__ArrayConcat(arr1: any[], ...args: any[]): any[] {
+  const out: any[] = [];
   for (let i = 0; i < arr1.length; i++) {
-    out[i] = arr1[i];
+    out[out.length] = arr1[i];
   }
-  for (let i = 0; i < arr2.length; i++) {
-    out[i] = arr2[i];
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    // Hack because we don't have an isArray function
+    if (pcall(() => (arg as any[]).length) && type(arg) !== "string") {
+        const argAsArray = (arg as any[]);
+        for (let j = 0; j < argAsArray.length; j++) {
+            out[out.length] = argAsArray[j];
+        }
+    } else {
+        out[out.length] = arg;
+    }
   }
+
   return out;
 }

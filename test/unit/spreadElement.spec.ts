a4cf0de752843a837dccf266ffcd6eb9c4f5c2bc
lolleko <lorenz.junglas@student.kit.edu>
2018-08-30 13:52:07 +0200

    Added spreadelement test for Lua 5.1

--- test/unit/spreadElement.spec.ts
+++ test/unit/spreadElement.spec.ts
@@ -1,5 +1,6 @@
 import { Expect, Test, TestCase } from "alsatian";
 
+import { LuaTarget } from "../../src/Transpiler";
 import * as util from "../src/util";
 
 export class SpreadElementTest {
@@ -12,5 +13,12 @@ export class SpreadElementTest {
         const lua = util.transpileString(`return JSONStringify([].push(...${JSON.stringify(inp)}));`);
         const result = util.executeLua(lua);
         Expect(result).toBe([].push(...inp));
+    }
+
+    @Test("Spread Element Lua 5.1")
+    public spreadElement51() {
+        // Cant test functional because our VM doesn't run on 5.1
+        const lua = util.transpileString(`[].push(...${JSON.stringify([1, 2, 3])});`, {luaTarget: LuaTarget.Lua51});
+        Expect(lua).toBe("__TS__ArrayPush({}, unpack({1,2,3}))");
     }
 }

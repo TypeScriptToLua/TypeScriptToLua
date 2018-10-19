34d3f4ad376861e9b60d9117e63f3d0c947dfbb0
Tom <26638278+tomblind@users.noreply.github.com>
2018-10-19 14:27:43 -0600

    refactored try block to allow rethrow (#251)
    * refactored try block to allow rethrow
    
    * updated rethrow test with something less convoluted

--- test/translation/lua/tryCatchFinally.lua
+++ test/translation/lua/tryCatchFinally.lua
@@ -1,7 +1,11 @@
-xpcall(function()
-    local a = 42;
-end,
-function(er)
-    local b = "fail";
-end)
-local c = "finally";
+do
+    local __TS_try, er = pcall(function()
+        local a = 42;
+    end);
+    if not __TS_try then
+        local b = "fail";
+    end
+    do
+        local c = "finally";
+    end
+end

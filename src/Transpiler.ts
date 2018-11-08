26fe3571d462f398826630191895bc0923b239a0
Lorenz Junglas <lolleko@users.noreply.github.com>
2018-11-08 11:00:58 +0100

    Fixed constructor overloads (#275)
    * Fixed constructor overloads
    
    Fixes #274
    
    * Removed lambda body
    
    * Removed transpileFile (should be added to changelog since this was exposed in the API)

--- src/Transpiler.ts
+++ src/Transpiler.ts
@@ -337,9 +337,7 @@ export abstract class LuaTranspiler {
 
     public transpileLuaLibFunction(func: LuaLibFeature, ...params: string[]): string {
         this.importLuaLibFeature(func);
-        params = params.filter(element => {
-            return element.toString() !== "";
-          });
+        params = params.filter(element => element.toString() !== "");
         return `__TS__${func}(${params.join(", ")})`;
     }
 
@@ -1778,8 +1776,9 @@ export abstract class LuaTranspiler {
             result += this.indent + `${className}.${fieldName} = ${value}\n`;
         }
 
-        // Try to find constructor
-        const constructor = node.members.filter(ts.isConstructorDeclaration)[0];
+        // Find first constructor with body
+        const constructor =
+            node.members.filter(n => ts.isConstructorDeclaration(n) && n.body)[0] as ts.ConstructorDeclaration;
         if (constructor) {
             // Add constructor plus initialization of instance fields
             result += this.transpileConstructor(constructor, className);

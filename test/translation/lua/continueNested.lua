2d19d27da69bb4869809bcea65ab3bb10508a922
doctorgester <doctorgester@gmail.com>
2018-12-28 03:13:40 +0300

    Fixed tests

--- test/translation/lua/continueNested.lua
+++ test/translation/lua/continueNested.lua
@@ -1,11 +1,11 @@
 local i = 0
-while(i<5) do
+while (i<5) do
     do
         if (i%2)==0 then
             goto __continue0
         end
         local j = 0
-        while(j<2) do
+        while (j<2) do
             do
                 if j==1 then
                     goto __continue1

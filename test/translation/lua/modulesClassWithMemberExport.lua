c664e5f3027290b13616a58b4fd63a6da4f614e2
Tom <26638278+tomblind@users.noreply.github.com>
2019-01-21 06:30:21 -0700

    corrected class export transform tests (which re-breaks them right now)

--- test/translation/lua/modulesClassWithMemberExport.lua
+++ test/translation/lua/modulesClassWithMemberExport.lua
@@ -1,15 +1,15 @@
 local exports = exports or {};
-exports.TestClass = TestClass or {};
-TestClass.__index = TestClass;
-TestClass.new = function(construct, ...)
-    local self = setmetatable({}, TestClass);
-    if construct and TestClass.constructor then
-        TestClass.constructor(self, ...);
+exports.TestClass = exports.TestClass or {};
+exports.TestClass.__index = exports.TestClass;
+exports.TestClass.new = function(construct, ...)
+    local self = setmetatable({}, exports.TestClass);
+    if construct and exports.TestClass.constructor then
+        exports.TestClass.constructor(self, ...);
     end
     return self;
 end;
-TestClass.constructor = function(self)
+exports.TestClass.constructor = function(self)
 end;
-TestClass.memberFunc = function(self)
+exports.TestClass.memberFunc = function(self)
 end;
 return exports;

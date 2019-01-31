be4c3400f583d1eee923b789bb9c9347319bbac7
hazzard993 <hazzard993@gmail.com>
2019-01-31 10:50:53 +1000

    Added tests for call expressions

--- test/unit/bindingpatterns.spec.ts
+++ test/unit/bindingpatterns.spec.ts
@@ -4,9 +4,7 @@ import * as util from "../src/util";
 
 const testCases = [
     ["{x}", "{x: true}", "x"],
-    ["{x = true}", "{}", "x"],
     ["{x, y}", "{x: false, y: true}", "y"],
-    ["{x, y = true}", "{x: false}", "y"],
     ["{x: foo, y}", "{x: true, y: false}", "foo"],
     ["{x: foo, y: bar}", "{x: false, y: true}", "bar"],
     ["{x: {x, y}, z}", "{x: {x: true, y: false}, z: false}", "x"],
@@ -14,11 +12,17 @@ const testCases = [
     ["{x: {x, y}, z}", "{x: {x: false, y: false}, z: true}", "z"],
 ];
 
+const testCasesDefault = [
+    ["{x = true}", "{}", "x"],
+    ["{x, y = true}", "{x: false}", "y"],
+];
+
 export class BindingPatternTests {
 
     @TestCase("{x, y}, z", "{x: false, y: false}, true", "z")
     @TestCase("{x, y}, {z}", "{x: false, y: false}, {z: true}", "z")
     @TestCases(testCases)
+    @TestCases(testCasesDefault)
     @Test("Object bindings in functions")
     public testObjectBindingPatternParameters(
         bindingString: string,
@@ -35,6 +39,7 @@ export class BindingPatternTests {
     }
 
     @TestCases(testCases)
+    @TestCases(testCasesDefault)
     public testObjectBindingPatternDeclarations(
         bindingString: string,
         objectString: string,
@@ -46,4 +51,22 @@ export class BindingPatternTests {
         `);
         Expect(result).toBe(true);
     }
+
+    @TestCases(testCases)
+    @Test("Object bindings with call expressions")
+    public testObjectBindingCallExpressions(
+        bindingString: string,
+        objectString: string,
+        returnVariable: string
+    ): void {
+        const result = util.transpileAndExecute(`
+            function call() {
+                return ${objectString};
+            }
+            let ${bindingString} = call();
+            return ${returnVariable};
+        `);
+        Expect(result).toBe(true);
+    }
+
 }

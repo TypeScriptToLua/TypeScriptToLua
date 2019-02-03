dd0f2528ddf9b856fc661dc83a01c2a3ddf68d20
Tobias Jahn <tobias.jahn@edutapps.de>
2019-02-03 02:25:41 +0100

    added __TS__Async and __TS__Await

---
+++ dist/lualib/Await.lua
@@ -0,0 +1,13 @@
+__TS__Await = function( value )
+  if type(value)=="table" and value.next then
+    local s,e
+    local r = coroutine.yield(value:next( nil, function( value )
+      s=true
+      e=value
+    end ))
+    if s then error(e,2) end
+    return r
+  else
+    return value
+  end
+end

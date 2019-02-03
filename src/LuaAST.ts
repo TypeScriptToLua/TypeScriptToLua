ffee0960003d645f81ca828f5bcf1db17d4f1078
Tobias Jahn <tobias.jahn@edutapps.de>
2019-02-03 15:26:25 +0100

    removed modifications to lua AST

--- src/LuaAST.ts
+++ src/LuaAST.ts
@@ -23,8 +23,6 @@ export enum SyntaxKind {
     BreakStatement,
     ExpressionStatement,
     // Expression
-    AwaitKeyword,
-    AsyncKeyword,
     AwaitExpression,
     StringLiteral,
     NumericLiteral,
@@ -439,22 +437,6 @@ export function createLabelStatement(name: string, tsOriginal?: ts.Node, parent?
     const statement = createNode(SyntaxKind.LabelStatement, tsOriginal, parent) as LabelStatement;
     statement.name = name;
     return statement;
-}
-
-
-export interface AwaitExpression extends Expression {
-    kind: SyntaxKind.AwaitExpression;
-    expression: UnaryExpression;
-}
-
-export function isAwaitExpression(node: Node): node is AwaitExpression {
-  return node.kind === SyntaxKind.AwaitExpression;
-}
-
-export function createAwaitExpression(expression: Expression): AwaitExpression {
-  const expr = createNode(SyntaxKind.AwaitExpression ) as AwaitExpression;
-  setParent(expression, expr);
-  return expr;
 }
 
 export interface ReturnStatement extends Statement {

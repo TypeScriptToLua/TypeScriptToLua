760d0871ee7e96b993ac6e916c9726bf1f0b07bd
Tom <26638278+tomblind@users.noreply.github.com>
2019-02-14 12:52:49 -0700

    tabs bad; spaces good

--- src/lualib/NewIndex.ts
+++ src/lualib/NewIndex.ts
@@ -1,37 +1,37 @@
 interface LuaClass {
-	prototype: LuaObject;
-	____super?: LuaClass;
+    prototype: LuaObject;
+    ____super?: LuaClass;
 }
 
 declare interface LuaObject {
-	constructor: LuaClass;
-	____setters?: { [key: string]: (self: LuaObject, val: any) => void };
+    constructor: LuaClass;
+    ____setters?: { [key: string]: (self: LuaObject, val: any) => void };
 }
 
 declare function rawget<T, K extends keyof T>(obj: T, key: K): T[K];
 declare function rawset<T, K extends keyof T>(obj: T, key: K, val: T[K]): void;
 
 function __TS__NewIndex(classProto: LuaObject): (tbl: LuaObject, key: keyof LuaObject, val: any) => void {
-	return (tbl, key, val) => {
-		let proto = classProto;
-		while (true) {
-			const setters = rawget(proto, "____setters");
-			if (setters) {
-				const setter = setters[key];
-				if (setter) {
-					setter(tbl, val);
-					return;
-				}
-			}
+    return (tbl, key, val) => {
+        let proto = classProto;
+        while (true) {
+            const setters = rawget(proto, "____setters");
+            if (setters) {
+                const setter = setters[key];
+                if (setter) {
+                    setter(tbl, val);
+                    return;
+                }
+            }
 
-			const base = rawget(rawget(proto, "constructor"), "____super");
-			if (!base) {
-				break;
-			}
+            const base = rawget(rawget(proto, "constructor"), "____super");
+            if (!base) {
+                break;
+            }
 
-			proto = rawget(base, "prototype");
-		}
+            proto = rawget(base, "prototype");
+        }
 
-		rawset(tbl, key, val);
-	};
+        rawset(tbl, key, val);
+    };
 }

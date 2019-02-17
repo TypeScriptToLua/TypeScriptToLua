76382753fd20bff0aa8f122bbb7a916c47420baf
Tom <26638278+tomblind@users.noreply.github.com>
2019-02-17 06:29:16 -0700

    fixed issue where for loop variable would override local with same name

--- test/translation/lua/continueConcurrent.lua
+++ test/translation/lua/continueConcurrent.lua
@@ -1,13 +1,15 @@
-local i = 0;
-while i < 10 do
-    do
-        if i < 5 then
-            goto __continue1;
+do
+    local i = 0;
+    while i < 10 do
+        do
+            if i < 5 then
+                goto __continue1;
+            end
+            if i == 7 then
+                goto __continue1;
+            end
         end
-        if i == 7 then
-            goto __continue1;
-        end
+        ::__continue1::
+        i = i + 1;
     end
-    ::__continue1::
-    i = i + 1;
 end

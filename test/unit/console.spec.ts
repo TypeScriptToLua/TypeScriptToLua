9dcf2416b693221e32c86c042054b3127eea0830
hazzard993 <hazzard993@gmail.com>
2019-02-26 15:48:54 +1000

    Seperated tests

--- test/unit/console.spec.ts
+++ test/unit/console.spec.ts
@@ -6,17 +6,38 @@ export class ConsoleTests {
     @TestCase("console.log()", "print();")
     @TestCase('console.log("Hello")', 'print("Hello");')
     @TestCase('console.log("Hello %s", "there")', 'print(string.format("Hello %s", "there"));')
+    @TestCase('console.log("Hello %%s", "there")', 'print(string.format("Hello %%s", "there"));')
     @TestCase('console.log("Hello", "There")', 'print("Hello", "There");')
+    @Test("console.log")
+    public testConsoleLog(inp: string, expected: string): void {
+        // Transpile
+        const lua = util.transpileString(inp);
+
+        // Assert
+        Expect(lua).toBe(expected);
+    }
+
     @TestCase("console.trace()", "print(debug.traceback());")
     @TestCase('console.trace("message")', 'print(debug.traceback("message"));')
     @TestCase('console.trace("Hello %s", "there")', 'print(debug.traceback(string.format("Hello %s", "there")));')
+    @TestCase('console.trace("Hello %%s", "there")', 'print(debug.traceback(string.format("Hello %%s", "there")));')
     @TestCase('console.trace("Hello", "there")', 'print(debug.traceback("Hello", "there"));')
+    @Test("console.trace")
+    public testConsoleTrace(inp: string, expected: string): void {
+        // Transpile
+        const lua = util.transpileString(inp);
+
+        // Assert
+        Expect(lua).toBe(expected);
+    }
+
     @TestCase("console.assert(false)", "assert(false);")
     @TestCase('console.assert(false, "message")', 'assert(false, "message");')
     @TestCase('console.assert(false, "message %s", "info")', 'assert(false, string.format("message %s", "info"));')
+    @TestCase('console.assert(false, "message %%s", "info")', 'assert(false, string.format("message %%s", "info"));')
     @TestCase('console.assert(false, "message", "more")', 'assert(false, "message", "more");')
-    @Test("Console")
-    public testConsoleCalls(inp: string, expected: string): void {
+    @Test("console.assert")
+    public testConsoleAssert(inp: string, expected: string): void {
         // Transpile
         const lua = util.transpileString(inp);
 

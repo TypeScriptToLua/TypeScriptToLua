382ebf2bd8e24b7881063b1487f681a7959259a4
hazzard993 <hazzard993@gmail.com>
2019-03-14 19:07:25 +1000

    AAA consideration and updated transpileString

--- test/unit/require.spec.ts
+++ test/unit/require.spec.ts
@@ -23,11 +23,11 @@ export class RequireTests {
         expectedPath: string,
         options: CompilerOptions,
         throwsError: boolean): void {
-        const regex = /require\("(.*?)"\)/;                 // This regex extracts `hello` from require("hello")
         if (throwsError) {
             Expect(() => util.transpileString(`import * from "${usedPath}";`, options, true, filePath)).toThrow();
         } else {
             const lua = util.transpileString(`import * from "${usedPath}";`, options, true, filePath);
+            const regex = /require\("(.*?)"\)/;
             const match = regex.exec(lua);
             Expect(match[1]).toBe(expectedPath);
         }
@@ -37,15 +37,11 @@ export class RequireTests {
     @TestCase("/** @noResolution */", "fake")
     @Test("noResolution on ambient modules causes no path alterations")
     public testRequireModule(comment: string, expectedPath: string): void {
+        const lua = util.transpileString({
+            "src/file.ts": `import * as fake from "fake";`,
+            "module.d.ts": `${comment} declare module "fake" {}`,
+        }, undefined, true, "src/file.ts");
         const regex = /require\("(.*?)"\)/;
-        const lua = util.transpileString(`
-            import * as fake from "fake";
-        `, undefined, true, "src/file.ts", {
-            "module.d.ts": `
-                ${comment}
-                declare module "fake" {}
-            `,
-        });
         Expect(regex.exec(lua)[1]).toBe(expectedPath);
     }
 

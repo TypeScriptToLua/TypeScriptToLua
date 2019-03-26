e5fa49a4ab83a393f4fe50d7929529749eaed365
ark120202 <ark120202@gmail.com>
2019-03-27 00:21:22 +0500

    Sort transformation test fixture names

--- test/translation/transformation.spec.ts
+++ test/translation/transformation.spec.ts
@@ -7,6 +7,7 @@ const fixturesPath = path.join(__dirname, "./transformation");
 const fixtures = fs
     .readdirSync(fixturesPath)
     .filter(f => path.extname(f) === ".ts")
+    .sort()
     .map(f => [path.parse(f).name, fs.readFileSync(path.join(fixturesPath, f), "utf8")]);
 
 test.each(fixtures)("Transformation (%s)", (_name, content) => {

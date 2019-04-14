7d034cae78bbf7601197a55919828fd46169373b
Perryvw <perry.wesel@gmail.com>
2019-04-14 16:30:19 +0200

    revert jest config changes

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const isCI = require("is-ci");
+
 /** @type {Partial<import("@jest/types").Config.DefaultOptions>} */
 module.exports = {
     testMatch: ["**/test/**/*.spec.ts"],
@@ -10,7 +12,7 @@ module.exports = {
     globals: {
         "ts-jest": {
             tsConfig: "<rootDir>/test/tsconfig.json",
-            diagnostics: { warnOnly: false },
+            diagnostics: { warnOnly: !isCI },
         },
     },
 };

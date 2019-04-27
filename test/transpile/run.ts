55acfd7875edc3048e56963beed2db03edd6c819
ark120202 <ark120202@gmail.com>
2019-04-28 01:56:49 +0500

    Use transpileFiles in transpile tests runner

--- test/transpile/run.ts
+++ test/transpile/run.ts
@@ -15,18 +15,10 @@ export function buildVirtualProject(
     options.skipLibCheck = true;
     options.types = [];
 
-    const program = ts.createProgram({ rootNames, options });
-    const { transpiledFiles, diagnostics: transpileDiagnostics } = tstl.transpile({ program });
-
-    const diagnostics = ts.sortAndDeduplicateDiagnostics([
-        ...ts.getPreEmitDiagnostics(program),
-        ...transpileDiagnostics,
-    ]);
-
-    const emitResult = tstl.emitTranspiledFiles(options, transpiledFiles);
+    const { diagnostics, emitResult } = tstl.transpileFiles(rootNames, options);
     const emittedFiles = emitResult
         .map(result => path.relative(__dirname, result.name).replace(/\\/g, "/"))
         .sort();
 
-    return { diagnostics: [...diagnostics], emitResult, emittedFiles };
+    return { diagnostics, emitResult, emittedFiles };
 }

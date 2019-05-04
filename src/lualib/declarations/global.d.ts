05de680354b506ab03382fcd21e29a4c37ccce55
ark120202 <ark120202@gmail.com>
2019-05-04 06:44:16 +0500

    Add noSelfInFile to global declorations file

--- src/lualib/declarations/global.d.ts
+++ src/lualib/declarations/global.d.ts
@@ -1,5 +1,6 @@
-declare function tonumber(this: void, value: any, base?: number): number | undefined;
+/** @noSelfInFile */
+
+declare function tonumber(value: any, base?: number): number | undefined;
 declare function type(
-    this: void,
     value: any
 ): "nil" | "number" | "string" | "boolean" | "table" | "function" | "thread" | "userdata";

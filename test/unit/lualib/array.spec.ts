e84966ecf07ac876418ca0fd1262aec333f1b3f9
Perryvw <perry.wesel@gmail.com>
2019-05-27 20:52:16 +0200

    Updated tests

--- test/unit/lualib/array.spec.ts
+++ test/unit/lualib/array.spec.ts
@@ -6,10 +6,10 @@ test.each([{ inp: [0, 1, 2, 3], expected: [1, 2, 3, 4] }])("forEach (%p)", ({ in
         arrTest.forEach((elem, index) => {
             arrTest[index] = arrTest[index] + 1;
         })
-        return JSONStringify(arrTest);`,
+        return JSONStringify(arrTest);`
     );
 
-    expect(result).toBe(JSON.stringify(expected));
+    expect(JSON.parse(result)).toEqual(expected);
 });
 
 test.each([
@@ -21,25 +21,25 @@ test.each([
         `let arrTest = ${JSON.stringify(inp)};
         return JSONStringify(arrTest.findIndex((elem, index) => {
             return elem === ${searchEl};
-        }));`,
+        }));`
     );
 
-    expect(result).toBe(expected);
+    expect(result).toEqual(expected);
 });
 
-test.each([
-    { inp: [0, 2, 4, 8], expected: 3, value: 8 },
-    { inp: [0, 2, 4, 8], expected: 1, value: 2 },
-])("array.findIndex[index] (%p)", ({ inp, expected, value }) => {
-    const result = util.transpileAndExecute(
-        `let arrTest = ${JSON.stringify(inp)};
+test.each([{ inp: [0, 2, 4, 8], expected: 3, value: 8 }, { inp: [0, 2, 4, 8], expected: 1, value: 2 }])(
+    "array.findIndex[index] (%p)",
+    ({ inp, expected, value }) => {
+        const result = util.transpileAndExecute(
+            `let arrTest = ${JSON.stringify(inp)};
         return JSONStringify(arrTest.findIndex((elem, index, arr) => {
             return index === ${expected} && arr[${expected}] === ${value};
-        }));`,
-    );
+        }));`
+        );
 
-    expect(result).toBe(expected);
-});
+        expect(result).toEqual(expected);
+    }
+);
 
 test.each([
     { inp: [], func: "x => x" },
@@ -49,11 +49,9 @@ test.each([
     { inp: [0, 1, 2, 3], func: "x => x+2" },
     { inp: [0, 1, 2, 3], func: "x => x%2 == 0 ? x + 1 : x - 1" },
 ])("array.map (%p)", ({ inp, func }) => {
-    const result = util.transpileAndExecute(
-        `return JSONStringify([${inp.toString()}].map(${func}))`,
-    );
+    const result = util.transpileAndExecute(`return JSONStringify([${inp.toString()}].map(${func}))`);
 
-    expect(result).toBe(JSON.stringify(inp.map(eval(func))));
+    expect(JSON.parse(result)).toEqual(inp.map(eval(func)));
 });
 
 test.each([
@@ -65,11 +63,9 @@ test.each([
     { inp: [0, 1, 2, 3], func: "() => true" },
     { inp: [0, 1, 2, 3], func: "() => false" },
 ])("array.filter (%p)", ({ inp, func }) => {
-    const result = util.transpileAndExecute(
-        `return JSONStringify([${inp.toString()}].filter(${func}))`,
-    );
+    const result = util.transpileAndExecute(`return JSONStringify([${inp.toString()}].filter(${func}))`);
 
-    expect(result).toBe(JSON.stringify(inp.filter(eval(func))));
+    expect(JSON.parse(result)).toEqual(inp.filter(eval(func)));
 });
 
 test.each([
@@ -78,11 +74,9 @@ test.each([
     { inp: [false, true, false], func: "x => x" },
     { inp: [true, true, true], func: "x => x" },
 ])("array.every (%p)", ({ inp, func }) => {
-    const result = util.transpileAndExecute(
-        `return JSONStringify([${inp.toString()}].every(${func}))`,
-    );
+    const result = util.transpileAndExecute(`return JSONStringify([${inp.toString()}].every(${func}))`);
 
-    expect(result).toBe(JSON.stringify(inp.every(eval(func))));
+    expect(JSON.parse(result)).toEqual(inp.every(eval(func)));
 });
 
 test.each([
@@ -91,11 +85,9 @@ test.each([
     { inp: [false, true, false], func: "x => x" },
     { inp: [true, true, true], func: "x => x" },
 ])("array.some (%p)", ({ inp, func }) => {
-    const result = util.transpileAndExecute(
-        `return JSONStringify([${inp.toString()}].some(${func}))`,
-    );
+    const result = util.transpileAndExecute(`return JSONStringify([${inp.toString()}].some(${func}))`);
 
-    expect(result).toBe(JSON.stringify(inp.some(eval(func))));
+    expect(JSON.parse(result)).toEqual(inp.some(eval(func)));
 });
 
 test.each([
@@ -107,11 +99,16 @@ test.each([
     { inp: [0, 1, 2, 3, 4, 5], start: 1, end: 3 },
     { inp: [0, 1, 2, 3, 4, 5], start: 3 },
 ])("array.slice (%p)", ({ inp, start, end }) => {
-    const result = util.transpileAndExecute(
-        `return JSONStringify([${inp.toString()}].slice(${start}, ${end}))`,
-    );
+    const result = util.transpileAndExecute(`return JSONStringify([${inp.toString()}].slice(${start}, ${end}))`);
 
-    expect(result).toBe(JSON.stringify(inp.slice(start, end)));
+    expect(JSON.parse(result)).toEqual(inp.slice(start, end));
+});
+
+test("array.slice no argument", () => {
+    const input = [2, 3, 4, 5];
+    const result = util.transpileAndExecute(`return JSONStringify(${JSON.stringify(input)}.slice())`);
+
+    expect(JSON.parse(result)).toEqual(input);
 });
 
 test.each([
@@ -120,17 +117,19 @@ test.each([
     { inp: [0, 1, 2, 3], start: 2, deleteCount: 2, newElements: [9, 10, 11] },
     { inp: [0, 1, 2, 3], start: 4, deleteCount: 1, newElements: [8, 9] },
     { inp: [0, 1, 2, 3], start: 4, deleteCount: 0, newElements: [8, 9] },
+    { inp: [0, 1, 2, 3], start: -2, deleteCount: 0, newElements: [8, 9] },
+    { inp: [0, 1, 2, 3], start: -3, deleteCount: 0, newElements: [8, 9] },
     { inp: [0, 1, 2, 3, 4, 5], start: 5, deleteCount: 9, newElements: [10, 11] },
     { inp: [0, 1, 2, 3, 4, 5], start: 3, deleteCount: 2, newElements: [3, 4, 5] },
 ])("array.splice[Insert] (%p)", ({ inp, start, deleteCount, newElements }) => {
     const result = util.transpileAndExecute(
         `let spliceTestTable = [${inp.toString()}];
         spliceTestTable.splice(${start}, ${deleteCount}, ${newElements});
-        return JSONStringify(spliceTestTable);`,
+        return JSONStringify(spliceTestTable);`
     );
 
     inp.splice(start, deleteCount, ...newElements);
-    expect(result).toBe(JSON.stringify(inp));
+    expect(JSON.parse(result)).toEqual(inp);
 });
 
 test.each([
@@ -139,6 +138,8 @@ test.each([
     { inp: [0, 1, 2, 3], start: 10, deleteCount: 1 },
     { inp: [0, 1, 2, 3], start: 4 },
     { inp: [0, 1, 2, 3, 4, 5], start: 3 },
+    { inp: [0, 1, 2, 3, 4, 5], start: -3 },
+    { inp: [0, 1, 2, 3, 4, 5], start: -2 },
     { inp: [0, 1, 2, 3, 4, 5], start: 2, deleteCount: 2 },
     { inp: [0, 1, 2, 3, 4, 5, 6, 7, 8], start: 5, deleteCount: 9, newElements: [10, 11] },
 ])("array.splice[Remove] (%p)", ({ inp, start, deleteCount, newElements = [] }) => {
@@ -147,22 +148,22 @@ test.each([
         result = util.transpileAndExecute(
             `let spliceTestTable = [${inp.toString()}];
            spliceTestTable.splice(${start}, ${deleteCount}, ${newElements});
-           return JSONStringify(spliceTestTable);`,
+           return JSONStringify(spliceTestTable);`
         );
     } else {
         result = util.transpileAndExecute(
             `let spliceTestTable = [${inp.toString()}];
            spliceTestTable.splice(${start});
-           return JSONStringify(spliceTestTable);`,
+           return JSONStringify(spliceTestTable);`
         );
     }
 
     if (deleteCount) {
         inp.splice(start, deleteCount, ...newElements);
-        expect(result).toBe(JSON.stringify(inp));
+        expect(JSON.parse(result)).toEqual(inp);
     } else {
         inp.splice(start);
-        expect(result).toBe(JSON.stringify(inp));
+        expect(JSON.parse(result)).toEqual(inp);
     }
 });
 
@@ -182,11 +183,11 @@ test.each([
 
     const result = util.transpileAndExecute(
         `let concatTestTable: any[] = ${JSON.stringify(arr)};
-        return JSONStringify(concatTestTable.concat(${argStr}));`,
+        return JSONStringify(concatTestTable.concat(${argStr}));`
     );
 
     const concatArr = arr.concat(...args);
-    expect(result).toBe(JSON.stringify(concatArr));
+    expect(JSON.parse(result)).toEqual(concatArr);
 });
 
 test.each([
@@ -206,11 +207,10 @@ test.each([
     }
     const result = util.transpileAndExecute(
         `let joinTestTable = ${JSON.stringify(inp)};
-        return joinTestTable.join(${separatorLua});`,
+        return joinTestTable.join(${separatorLua});`
     );
 
-    const joinedInp = inp.join(separator);
-    expect(result).toBe(joinedInp);
+    expect(result).toEqual(inp.join(separator));
 });
 
 test.each([
@@ -230,7 +230,7 @@ test.each([
     const result = util.transpileAndExecute(str);
 
     // Account for lua indexing (-1)
-    expect(result).toBe(inp.indexOf(element, fromIndex));
+    expect(result).toEqual(inp.indexOf(element, fromIndex));
 });
 
 test.each([{ inp: [1, 2, 3], expected: 3 }, { inp: [1, 2, 3, 4, 5], expected: 3 }])(
@@ -238,46 +238,46 @@ test.each([{ inp: [1, 2, 3], expected: 3 }, { inp: [1, 2, 3, 4, 5], expected: 3 
     ({ inp, expected }) => {
         const result = util.transpileAndExecute(
             `let [x, y, z] = ${JSON.stringify(inp)}
-            return z;`,
+            return z;`
         );
 
-        expect(result).toBe(expected);
-    },
+        expect(result).toEqual(expected);
+    }
 );
 
 test.each([{ inp: [1] }, { inp: [1, 2, 3] }])("array.push (%p)", ({ inp }) => {
     const result = util.transpileAndExecute(
         `let testArray = [0];
         testArray.push(${inp.join(", ")});
-        return JSONStringify(testArray);`,
+        return JSONStringify(testArray);`
     );
 
-    expect(result).toBe(JSON.stringify([0].concat(inp)));
+    expect(JSON.parse(result)).toEqual([0].concat(inp));
 });
 
-test.each([
-    { array: "[1, 2, 3]", expected: [3, 2] },
-    { array: "[1, 2, 3, null]", expected: [3, 2] },
-])("array.pop (%p)", ({ array, expected }) => {
-    {
-        const result = util.transpileAndExecute(
-            `let testArray = ${array};
-            let val = testArray.pop();
-            return val`,
-        );
+test.each([{ array: "[1, 2, 3]", expected: [3, 2] }, { array: "[1, 2, 3, null]", expected: [3, 2] }])(
+    "array.pop (%p)",
+    ({ array, expected }) => {
+        {
+            const result = util.transpileAndExecute(
+                `let testArray = ${array};
+                let val = testArray.pop();
+                return val`
+            );
 
-        expect(result).toBe(expected[0]);
-    }
-    {
-        const result = util.transpileAndExecute(
-            `let testArray = ${array};
-            testArray.pop();
-            return testArray.length`,
-        );
+            expect(result).toEqual(expected[0]);
+        }
+        {
+            const result = util.transpileAndExecute(
+                `let testArray = ${array};
+                testArray.pop();
+                return testArray.length`
+            );
 
-        expect(result).toBe(expected[1]);
+            expect(result).toEqual(expected[1]);
+        }
     }
-});
+);
 
 test.each([
     { array: "[1, 2, 3]", expected: [3, 2, 1] },
@@ -289,9 +289,9 @@ test.each([
     const result = util.transpileAndExecute(
         `let testArray = ${array};
         let val = testArray.reverse();
-        return JSONStringify(testArray)`,
+        return JSONStringify(testArray)`
     );
-    expect(result).toBe(JSON.stringify(expected));
+    expect(JSON.parse(result)).toEqual(expected);
 });
 
 test.each([
@@ -305,19 +305,19 @@ test.each([
             const result = util.transpileAndExecute(
                 `let testArray = ${array};
                 let val = testArray.shift();
-                return JSONStringify(testArray)`,
+                return JSONStringify(testArray)`
             );
-            expect(result).toBe(JSON.stringify(expectedArray));
+            expect(JSON.parse(result)).toEqual(expectedArray);
         }
         // test return value
         {
             const result = util.transpileAndExecute(
                 `let testArray = ${array};
                 let val = testArray.shift();
-                return val`,
+                return val`
             );
 
-            expect(result).toBe(expectedValue);
+            expect(result).toEqual(expectedValue);
         }
     }
 });
@@ -331,10 +331,10 @@ test.each([
     const result = util.transpileAndExecute(
         `let testArray = ${array};
         testArray.unshift(${toUnshift});
-        return JSONStringify(testArray)`,
+        return JSONStringify(testArray)`
     );
 
-    expect(result).toBe(JSON.stringify(expected));
+    expect(JSON.parse(result)).toEqual(expected);
 });
 
 test.each([
@@ -346,10 +346,10 @@ test.each([
     const result = util.transpileAndExecute(
         `let testArray = ${array};
         testArray.sort();
-        return JSONStringify(testArray)`,
+        return JSONStringify(testArray)`
     );
 
-    expect(result).toBe(JSON.stringify(expected));
+    expect(JSON.parse(result)).toEqual(expected);
 });
 
 test.each([
@@ -371,10 +371,10 @@ test.each([
         return JSONStringify(testArray)`,
         undefined,
         undefined,
-        `declare function tonumber(this: void, e: any): number`,
+        `declare function tonumber(this: void, e: any): number`
     );
 
-    expect(result).toBe(JSON.stringify(array.sort(compareFn)));
+    expect(JSON.parse(result)).toEqual(array.sort(compareFn));
 });
 
 test.each([
@@ -440,8 +440,7 @@ test.each([
 
 test("array reduce index & array arguments (%p)", () => {
     const array = [1, 3, 5, 7];
-    const reducer = (total: number, _: number, index: number, array: number[]) =>
-        total + array[index];
+    const reducer = (total: number, _: number, index: number, array: number[]) => total + array[index];
 
     const result = util.transpileAndExecute(`
         const myArray = ${JSON.stringify(array)};

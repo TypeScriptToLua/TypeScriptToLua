0302c6935d06749f0769a9bfb74e7747e2e36628
ark120202 <ark120202@gmail.com>
2019-07-14 02:35:59 +0500

    Transform tagged template literals tests

--- test/unit/templateLiterals.spec.ts
+++ test/unit/templateLiterals.spec.ts
@@ -1,12 +1,5 @@
 import * as util from "../util";
 
-test.each(["a++", "a--", "--a", "++a"])("Template string expression (%p)", lambda => {
-    util.testFunction`
-        let a = 3;
-        return \`val\${${lambda}}\`;
-    `.expectToMatchJsResult();
-});
-
 test.each([
     { a: 12, b: 23, c: 43 },
     { a: "test", b: "hello", c: "bye" },
@@ -14,133 +7,53 @@ test.each([
     { a: "test", b: 42, c: 12 },
     { a: "test", b: 42, c: true },
     { a: false, b: 42, c: 12 },
-])("Template Strings (%p)", ({ a, b, c }) => {
-    util.testFunctionTemplate`
-        let a = ${a};
-        let b = ${b};
-        let c = ${c};
-        return \`${a} ${b} test ${c}\`;
+])("template literal (%p)", ({ a, b, c }) => {
+    util.testExpressionTemplate`\`\${${a}} \${${b}} test \${${c}}\``.expectToMatchJsResult();
+});
+
+test.each(["a++", "a--", "--a", "++a"])("template literal with expression (%p)", expression => {
+    util.testFunction`
+        let a = 3;
+        return \`value\${${expression}}\`;
     `.expectToMatchJsResult();
 });
 
-const testCases = [
-    {
-        callExpression: "func``",
-        joinAllResult: "",
-        joinRawResult: "",
-    },
-    {
-        callExpression: "func`hello`",
-        joinAllResult: "hello",
-        joinRawResult: "hello",
-    },
-    {
-        callExpression: "func`hello ${1} ${2} ${3}`",
-        joinAllResult: "hello 1 2 3",
-        joinRawResult: "hello   ",
-    },
-    {
-        callExpression: "func`hello ${(() => 'iife')()}`",
-        joinAllResult: "hello iife",
-        joinRawResult: "hello ",
-    },
-    {
-        callExpression: "func`hello ${1 + 2 + 3} arithmetic`",
-        joinAllResult: "hello 6 arithmetic",
-        joinRawResult: "hello  arithmetic",
-    },
-    {
-        callExpression: "func`begin ${'middle'} end`",
-        joinAllResult: "begin middle end",
-        joinRawResult: "begin  end",
-    },
-    {
-        callExpression: "func`hello ${func`hello`}`",
-        joinAllResult: "hello hello",
-        joinRawResult: "hello ",
-    },
-    {
-        callExpression: "func`hello \\u00A9`",
-        joinAllResult: "hello ©",
-        joinRawResult: "hello \\u00A9",
-    },
-    {
-        callExpression: "func`hello $ { }`",
-        joinAllResult: "hello $ { }",
-        joinRawResult: "hello $ { }",
-    },
-    {
-        callExpression: "func`hello { ${'brackets'} }`",
-        joinAllResult: "hello { brackets }",
-        joinRawResult: "hello {  }",
-    },
-    {
-        callExpression: "func`hello \\``",
-        joinAllResult: "hello `",
-        joinRawResult: "hello \\`",
-    },
-    {
-        callExpression: "obj.func`hello ${'propertyAccessExpression'}`",
-        joinAllResult: "hello propertyAccessExpression",
-        joinRawResult: "hello ",
-    },
-    {
-        callExpression: "obj['func']`hello ${'elementAccessExpression'}`",
-        joinAllResult: "hello elementAccessExpression",
-        joinRawResult: "hello ",
-    },
-];
+test.each([
+    "func``",
+    "func`hello`",
+    "func`hello ${1} ${2} ${3}`",
+    "func`hello ${(() => 'iife')()}`",
+    "func`hello ${1 + 2 + 3} arithmetic`",
+    "func`begin ${'middle'} end`",
+    "func`hello ${func`hello`}`",
+    "func`hello \\u00A9`",
+    "func`hello $ { }`",
+    "func`hello { ${'brackets'} }`",
+    "func`hello \\``",
+    "obj.func`hello ${'propertyAccessExpression'}`",
+    "obj['func']`hello ${'elementAccessExpression'}`",
+])("tagged template literal (%p)", expression => {
+    // TODO: https://github.com/TypeScriptToLua/TypeScriptToLua/issues/663
+    util.testFunction`
+        function func(strings: TemplateStringsArray, ...expressions: any[]) {
+            return { strings: [...strings], raw: strings.raw, expressions: expressions };
+        }
 
-test.each(testCases)("TaggedTemplateLiteral call (%p)", ({ callExpression, joinAllResult }) => {
-    const result = util.transpileAndExecute(`
-            function func(strings: TemplateStringsArray, ...expressions: any[]) {
-                const toJoin = [];
-                for (let i = 0; i < strings.length; ++i) {
-                    if (strings[i]) {
-                        toJoin.push(strings[i]);
-                    }
-                    if (expressions[i]) {
-                        toJoin.push(expressions[i]);
-                    }
-                }
-                return toJoin.join("");
-            }
-            const obj = {
-                func
-            };
-            return ${callExpression};
-        `);
-
-    expect(result).toBe(joinAllResult);
-});
-
-test.each(testCases)("TaggedTemplateLiteral raw preservation (%p)", ({ callExpression, joinRawResult }) => {
-    const result = util.transpileAndExecute(`
-            function func(strings: TemplateStringsArray, ...expressions: any[]) {
-                return strings.raw.join("");
-            }
-            const obj = {
-                func
-            };
-            return ${callExpression};
-        `);
-
-    expect(result).toBe(joinRawResult);
+        const obj = { func };
+        return ${expression};
+    `.expectToMatchJsResult();
 });
 
 test.each(["func`noSelfParameter`", "obj.func`noSelfParameter`", "obj[`func`]`noSelfParameter`"])(
-    "TaggedTemplateLiteral no self parameter",
-    callExpression => {
-        const result = util.transpileAndExecute(`
-            function func(this: void, strings: TemplateStringsArray, ...expressions: any[]) {
-                return strings.join("");
+    "tagged template literal function context (%p)",
+    expression => {
+        util.testFunction`
+            function func(this: void, strings: TemplateStringsArray) {
+                return [...strings];
             }
-            const obj = {
-                func
-            };
-            return ${callExpression};
-        `);
 
-        expect(result).toBe("noSelfParameter");
+            const obj = { func };
+            return ${expression};
+        `.expectToMatchJsResult();
     }
 );

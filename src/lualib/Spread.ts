54c76fabceb80cd91dbf051b2abfb7c2d6de4108
hazzard993 <hazzard993@gmail.com>
2019-07-28 20:07:20 +1000

    Use for loop for spreading strings

--- src/lualib/Spread.ts
+++ src/lualib/Spread.ts
@@ -1,8 +1,8 @@
-function __TS__Spread<T>(this: void, iterable: Iterable<T>): T[] {
+function __TS__Spread<T>(this: void, iterable: string | Iterable<T>): T[] {
     const arr = [];
     if (typeof iterable === "string") {
-        for (const [character] of string.gmatch(iterable, ".")) {
-            arr[arr.length] = character;
+        for (let i = 0; i < iterable.length; i += 1) {
+            arr[arr.length] = iterable[i];
         }
     } else {
         for (const item of iterable) {

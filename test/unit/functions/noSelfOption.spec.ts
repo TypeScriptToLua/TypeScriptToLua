ad87fd65ce34f675ea2db2fbf2997fa555d23e05
hazzard993 <hazzard993@gmail.com>
2019-09-07 16:22:28 +1000

    Neaten up noSelf test names

--- test/unit/functions/noSelfOption.spec.ts
+++ test/unit/functions/noSelfOption.spec.ts
@@ -3,56 +3,50 @@ import * as util from "../../util";
 test.each([
     ["foo: string, bar: string", `"foo", "bar"`, "foobar"],
     ["this: any, foo: string, bar: string", `"foo", "bar"`, "barnil"],
-])(
-    "enables noSelfInFile behaviour for functions (%s)",
-    (expectedParameters, suppliedParameters, result) => {
-        const transpiledCode = util.transpileString(
-            `function fooBar(${expectedParameters}) {
+])("enables noSelfInFile behaviour for functions (%s)", (expectedParameters, suppliedParameters, result) => {
+    const transpiledCode = util.transpileString(
+        `function fooBar(${expectedParameters}) {
             return foo + bar;
         }`,
-            {
-                noSelf: true,
-            }
-        );
+        {
+            noSelf: true,
+        }
+    );
 
-        const lua = `
+    const lua = `
         ${transpiledCode}
         return fooBar(${suppliedParameters})
     `;
 
-        expect(util.executeLua(lua)).toBe(result);
-    }
-);
+    expect(util.executeLua(lua)).toBe(result);
+});
 
 test.each([
     ["foo: string, bar: string", `"foo", "bar"`, "barnil"],
     ["this: void, foo: string, bar: string", `"foo", "bar"`, "foobar"],
     ["foo: string, bar: string", `fooBar, "foo", "bar"`, "foobar"],
-])(
-    "enables noSelfInFile behaviour for methods (%s)",
-    (expectedParameters, suppliedParameters, result) => {
-        const transpiledCode = util.transpileString(
-            `class FooBar {
+])("enables noSelfInFile behaviour for methods (%s)", (expectedParameters, suppliedParameters, result) => {
+    const transpiledCode = util.transpileString(
+        `class FooBar {
             fooBar(${expectedParameters}) {
                 return foo + bar;
             }
         }
         const fooBar = new FooBar();`,
-            {
-                noSelf: true,
-            }
-        );
+        {
+            noSelf: true,
+        }
+    );
 
-        const lua = `
+    const lua = `
         ${transpiledCode}
         return fooBar.fooBar(${suppliedParameters})
     `;
 
-        expect(util.executeLua(lua)).toBe(result);
-    }
-);
+    expect(util.executeLua(lua)).toBe(result);
+});
 
-test("noSelf option generates declaration files with @noSelfInFile", () => {
+test("generates declaration files with @noSelfInFile", () => {
     const result = util.transpileStringsAsProject(
         {
             "main.ts": `function fooBar(foo: string, bar: string) {

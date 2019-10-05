de818bcaf407dc4cb5f344882856bdb654c860fb
Perry van Wesel <Perryvw@users.noreply.github.com>
2019-10-05 18:29:54 +0200

    Added support for array.find (#730)
    * Added support for array.find
    
    * Changed lualib function signature to match TS lib

---
+++ src/lualib/ArrayFind.ts
@@ -0,0 +1,18 @@
+// https://www.ecma-international.org/ecma-262/10.0/index.html#sec-array.prototype.find
+function __TS__ArrayFind<T>(
+    this: void,
+    arr: T[],
+    predicate: (value: T, index: number, obj: T[]) => unknown
+): T | undefined {
+    const len = arr.length;
+    let k = 0;
+    while (k < len) {
+        const elem = arr[k];
+        if (predicate(elem, k, arr)) {
+            return elem;
+        }
+        k = k + 1;
+    }
+
+    return undefined;
+}

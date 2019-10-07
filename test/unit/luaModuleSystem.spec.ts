c6447c43ddf765bf42caec3c329a14f857035d48
hazzard993 <hazzard993@gmail.com>
2019-10-07 13:13:31 +1000

    Improve formatting of luaModuleSystem test

--- test/unit/luaModuleSystem.spec.ts
+++ test/unit/luaModuleSystem.spec.ts
@@ -2,35 +2,15 @@ import * as util from "../util";
 import * as ts from "typescript";
 import { LuaModuleSystemKind } from "../../src";
 
-test.each<[string, Record<string, string>]>([
-    [
-        "Import module without package or require",
-        {
-            "main.ts": `
-                import { value } from "./module";
-                if (value !== true) {
-                    throw "Failed to import value";
-                }
-            `,
-            "module.ts": `
-                export const value = true;
-            `,
-        },
-    ],
-])("luaModuleSystem with outFile (%s)", (_, files) => {
-    const testBuilder = util.testBundle`
-        ${files["main.ts"]}
+test("luaModuleSystem with outFile prevents re-creates require and package", () => {
+    util.testBundle`
+        import { value } from "./module";
+        if (value !== true) {
+            throw "Failed to import value";
+        }
     `
         .setOptions({ outFile: "main.lua", module: ts.ModuleKind.AMD, luaModuleSystem: LuaModuleSystemKind.None })
-        .setModuleSystem("none");
-
-    const extraFiles = Object.keys(files)
-        .map(file => ({ fileName: file, code: files[file] }))
-        .filter(file => file.fileName !== "main.ts");
-
-    extraFiles.forEach(extraFile => {
-        testBuilder.addExtraFile(extraFile.fileName, extraFile.code);
-    });
-
-    testBuilder.expectNoExecutionError();
+        .setModuleSystem("none")
+        .addExtraFile("module.ts", "export const value = true;")
+        .expectNoExecutionError();
 });

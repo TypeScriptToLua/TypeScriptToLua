b2ab7db223ea4c5f56b769ddc635788534956a3b
hazzard993 <hazzard993@gmail.com>
2019-10-16 18:52:44 +1000

    Remove describe from outFile tests

--- test/unit/outFile.spec.ts
+++ test/unit/outFile.spec.ts
@@ -11,123 +11,121 @@ function outFileOptionsWithEntry(luaEntry: string): CompilerOptions {
     return { outFile: "main.lua", noHoisting: true, module: ts.ModuleKind.AMD, luaEntry };
 }
 
-describe("outFile", () => {
-    test("import module -> main", () => {
-        util.testBundle`
-            import { value } from "./module";
-            ${exportResultSource}
-        `
-            .addExtraFile("module.ts", exportValueSource)
-            .setOptions(outFileOptionsWithEntry("main.ts"))
-            .expectToEqual({ result: true });
-    });
+test("import module -> main", () => {
+    util.testBundle`
+        import { value } from "./module";
+        ${exportResultSource}
+    `
+        .addExtraFile("module.ts", exportValueSource)
+        .setOptions(outFileOptionsWithEntry("main.ts"))
+        .expectToEqual({ result: true });
+});
 
-    test("import chain export -> reexport -> main", () => {
-        util.testBundle`
-            import { value } from "./reexport";
-            ${exportResultSource}
-        `
-            .addExtraFile("reexport.ts", reexportValueSource)
-            .addExtraFile("export.ts", exportValueSource)
-            .setOptions(outFileOptionsWithEntry("main.ts"))
-            .expectToEqual({ result: true });
-    });
+test("import chain export -> reexport -> main", () => {
+    util.testBundle`
+        import { value } from "./reexport";
+        ${exportResultSource}
+    `
+        .addExtraFile("reexport.ts", reexportValueSource)
+        .addExtraFile("export.ts", exportValueSource)
+        .setOptions(outFileOptionsWithEntry("main.ts"))
+        .expectToEqual({ result: true });
+});
 
-    test("diamond imports/exports -> reexport1 & reexport2 -> main", () => {
-        util.testBundle`
-            import { value as a } from "./reexport1";
-            import { value as b } from "./reexport2";
-            if (a !== true || b !== true) {
-                throw "Failed to import a or b";
-            }
-        `
-            .addExtraFile("reexport1.ts", reexportValueSource)
-            .addExtraFile("reexport2.ts", reexportValueSource)
-            .addExtraFile("export.ts", exportValueSource)
-            .setOptions(outFileOptionsWithEntry("main.ts"))
-            .expectNoExecutionError();
-    });
+test("diamond imports/exports -> reexport1 & reexport2 -> main", () => {
+    util.testBundle`
+        import { value as a } from "./reexport1";
+        import { value as b } from "./reexport2";
+        if (a !== true || b !== true) {
+            throw "Failed to import a or b";
+        }
+    `
+        .addExtraFile("reexport1.ts", reexportValueSource)
+        .addExtraFile("reexport2.ts", reexportValueSource)
+        .addExtraFile("export.ts", exportValueSource)
+        .setOptions(outFileOptionsWithEntry("main.ts"))
+        .expectNoExecutionError();
+});
 
-    test("module in directory", () => {
-        util.testBundle`
-            import { value } from "./module/module";
-            ${exportValueSource}
-        `
-            .addExtraFile("module/module.ts", exportValueSource)
-            .setOptions(outFileOptionsWithEntry("main.ts"))
-            .expectNoExecutionError();
-    });
+test("module in directory", () => {
+    util.testBundle`
+        import { value } from "./module/module";
+        ${exportValueSource}
+    `
+        .addExtraFile("module/module.ts", exportValueSource)
+        .setOptions(outFileOptionsWithEntry("main.ts"))
+        .expectNoExecutionError();
+});
 
-    test("modules aren't ordered by name", () => {
-        util.testBundle`
-            import { value } from "./a";
-            ${exportResultSource}
-        `
-            .addExtraFile("a.ts", exportValueSource)
-            .setOptions(outFileOptionsWithEntry("main.ts"))
-            .expectToEqual({ result: true });
-    });
+test("modules aren't ordered by name", () => {
+    util.testBundle`
+        import { value } from "./a";
+        ${exportResultSource}
+    `
+        .addExtraFile("a.ts", exportValueSource)
+        .setOptions(outFileOptionsWithEntry("main.ts"))
+        .expectToEqual({ result: true });
+});
 
-    test("entry point in directory", () => {
-        util.testBundle``
-            .addExtraFile(
-                "main/main.ts",
-                `
-                    import { value } from "../module";
-                    ${exportResultSource}
-                `
-            )
-            .addExtraFile("module.ts", exportValueSource)
-            .setOptions(outFileOptionsWithEntry("main/main.ts"))
-            .expectToEqual({ result: true });
-    });
+test("entry point in directory", () => {
+    util.testBundle``
+        .addExtraFile(
+            "main/main.ts",
+            `
+                import { value } from "../module";
+                ${exportResultSource}
+            `
+        )
+        .addExtraFile("module.ts", exportValueSource)
+        .setOptions(outFileOptionsWithEntry("main/main.ts"))
+        .expectToEqual({ result: true });
+});
 
-    test("LuaLibs", () => {
-        util.testBundle`
-            import { array } from "./module";
-            if (array[2] !== 3) {
-                throw "Array's third item is not three";
-            }
-        `
-            .addExtraFile(
-                "module.ts",
-                `
-                    export const array = [1, 2];
-                    array.push(3);
-                `
-            )
-            .setOptions(outFileOptionsWithEntry("main.ts"))
-            .expectNoExecutionError();
-    });
+test("LuaLibs", () => {
+    util.testBundle`
+        import { array } from "./module";
+        if (array[2] !== 3) {
+            throw "Array's third item is not three";
+        }
+    `
+        .addExtraFile(
+            "module.ts",
+            `
+                export const array = [1, 2];
+                array.push(3);
+            `
+        )
+        .setOptions(outFileOptionsWithEntry("main.ts"))
+        .expectNoExecutionError();
+});
 
-    test("cyclic imports", () => {
-        util.testBundle`
-            export const a = true;
-            import { value } from "./b";
-            ${exportResultSource}
-        `
-            .addExtraFile(
-                "b.ts",
-                `
-                    import { a } from "./main";
-                    export const value = a;
-                `
-            )
-            .setOptions(outFileOptionsWithEntry("main.ts"))
-            .expectToEqual({ a: true, result: true });
-    });
+test("cyclic imports", () => {
+    util.testBundle`
+        export const a = true;
+        import { value } from "./b";
+        ${exportResultSource}
+    `
+        .addExtraFile(
+            "b.ts",
+            `
+                import { a } from "./main";
+                export const value = a;
+            `
+        )
+        .setOptions(outFileOptionsWithEntry("main.ts"))
+        .expectToEqual({ a: true, result: true });
+});
 
-    test("luaEntry doesn't exist", () => {
-        util.testBundle``
-            .setOptions(outFileOptionsWithEntry("entry.ts"))
-            .expectToHaveDiagnosticOfError(TSTLErrors.LuaEntryNotFound("entry.ts"));
-    });
+test("luaEntry doesn't exist", () => {
+    util.testBundle``
+        .setOptions(outFileOptionsWithEntry("entry.ts"))
+        .expectToHaveDiagnosticOfError(TSTLErrors.LuaEntryNotFound("entry.ts"));
+});
 
-    test("luaEntry resolved from path specified in tsconfig", () => {
-        util.testBundle``
-            .addExtraFile("src/main.ts", "")
-            .addExtraFile("src/module.ts", "")
-            .setOptions({ rootDir: "src", ...outFileOptionsWithEntry("src/main.ts") })
-            .expectToHaveNoDiagnostics();
-    });
+test("luaEntry resolved from path specified in tsconfig", () => {
+    util.testBundle``
+        .addExtraFile("src/main.ts", "")
+        .addExtraFile("src/module.ts", "")
+        .setOptions({ rootDir: "src", ...outFileOptionsWithEntry("src/main.ts") })
+        .expectToHaveNoDiagnostics();
 });

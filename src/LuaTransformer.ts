a9a94d3ef9dda4ab6728c896cb84f2b2e78a91be
hazzard993 <hazzard993@gmail.com>
2019-10-18 19:14:34 +1000

    Check for configFilePath for luaEntry, not project

--- src/LuaTransformer.ts
+++ src/LuaTransformer.ts
@@ -128,7 +128,7 @@ export class LuaTransformer {
         if (this.options.luaEntry) {
             // If specified via CLI, the program's host should pick up a relative luaEntry path
             let luaEntryPath = this.options.luaEntry;
-            if (this.options.project) {
+            if (this.options.configFilePath) {
                 // If specified via project. Figure out the full path to the file based off the project's path
                 const configFileName = this.options.configFilePath as string | undefined;
                 const basedir = configFileName ? path.dirname(configFileName) : process.cwd();

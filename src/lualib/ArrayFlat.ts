50190982b1f10a03e1836a4504c9a5b8b3c24961
Radosław Miernik <radekmie@gmail.com>
2019-10-22 21:03:07 +0200

    Added a clean workaround for next.

--- src/lualib/ArrayFlat.ts
+++ src/lualib/ArrayFlat.ts
@@ -1,8 +1,11 @@
 function __TS__ArrayFlat(this: void, array: any[], depth = 1): any[] {
     let result: any[] = [];
     for (const value of array) {
-        // A hack to concat only true "arrays" and empty tables.
-        if (depth > 0 && type(value) === "table" && (1 in value || next(value, undefined) === undefined)) {
+        if (
+            depth > 0 &&
+            type(value) === "table" &&
+            (1 in value || (next as NextEmptyCheck)(value, undefined) === undefined)
+        ) {
             result = result.concat(__TS__ArrayFlat(value, depth - 1));
         } else {
             result[result.length] = value;
--- src/lualib/ArrayFlatMap.ts
+++ src/lualib/ArrayFlatMap.ts
@@ -6,8 +6,10 @@ function __TS__ArrayFlatMap<T, U>(
     let result: U[] = [];
     for (let i = 0; i < array.length; i++) {
         const value = callback(array[i], i, array);
-        // A hack to concat only true "arrays" and empty tables.
-        if (type(value) === "table" && (1 in value || next(value as any, undefined) === undefined)) {
+        if (
+            type(value) === "table" &&
+            (1 in value || (next as NextEmptyCheck)(value as any, undefined) === undefined)
+        ) {
             result = result.concat(value);
         } else {
             result[result.length] = value as U;

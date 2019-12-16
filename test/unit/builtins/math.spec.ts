eeaca6a771c65c404d44f56a8ffe103a89a87876
hazzard993 <hazzard993@gmail.com>
2019-12-16 19:41:44 +1000

    Add Math.atan2 Lua5.3 comparison test, fix math.atan taps

--- test/unit/builtins/math.spec.ts
+++ test/unit/builtins/math.spec.ts
@@ -25,12 +25,16 @@ test.each(["E", "LN10", "LN2", "LOG10E", "LOG2E", "SQRT1_2", "SQRT2"])("Math.%s"
     });
 });
 
-const expectMathAtan2: util.TapCallback = builder => expect(builder.getMainLuaCodeChunk()).toMatch("math.atan2");
-const expectMathAtan: util.TapCallback = builder => expect(builder.getMainLuaCodeChunk()).toContain("math.atan");
+const expectMathAtan2: util.TapCallback = builder => expect(builder.getMainLuaCodeChunk()).toContain("math.atan2(");
+const expectMathAtan: util.TapCallback = builder => expect(builder.getMainLuaCodeChunk()).toContain("math.atan(");
 
 util.testEachVersion("Math.atan2", () => util.testExpression`Math.atan2(4, 5)`, {
     [tstl.LuaTarget.LuaJIT]: builder => builder.tap(expectMathAtan2),
     [tstl.LuaTarget.Lua51]: builder => builder.tap(expectMathAtan2),
     [tstl.LuaTarget.Lua52]: builder => builder.tap(expectMathAtan2),
     [tstl.LuaTarget.Lua53]: builder => builder.tap(expectMathAtan),
+});
+
+test("Lua 5.3 Math.atan2 comparison", () => {
+    util.testExpression`Math.atan2(4, 5)`.setOptions({ luaTarget: tstl.LuaTarget.Lua53 }).expectToMatchJsResult();
 });

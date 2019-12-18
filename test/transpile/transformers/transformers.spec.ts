d1a02914c0b55accdfaa59ab5a12255501bc13fc
ark120202 <ark120202@gmail.com>
2019-12-18 06:42:42 +0000

    Refactor `transformers.spec.ts` test

---
+++ test/transpile/transformers/transformers.spec.ts
@@ -0,0 +1,67 @@
+import * as path from "path";
+import * as tstl from "../../../src";
+import * as util from "../../util";
+
+const optionsOfTransformer = (transformer: tstl.TransformerImport): tstl.CompilerOptions => ({
+    plugins: [transformer],
+});
+
+test("should ignore language service plugins", () => {
+    util.testFunction`
+        return;
+    `
+        .setOptions({ plugins: [{ name: path.join(__dirname, "resolve.ts") }] })
+        .expectToEqual(undefined);
+});
+
+describe("resolution", () => {
+    const testTransform = (transformer: tstl.TransformerImport) => {
+        util.testFunction`
+            return;
+        `
+            .setOptions(optionsOfTransformer(transformer))
+            .expectToEqual(true);
+    };
+
+    test("resolve relative transformer paths", () => {
+        jest.spyOn(process, "cwd").mockReturnValue(__dirname);
+        testTransform({ transform: "./resolve.ts" });
+    });
+
+    test("load .js transformers", () => {
+        testTransform({ transform: path.join(__dirname, "resolve.js") });
+    });
+
+    test("load .ts transformers", () => {
+        testTransform({ transform: path.join(__dirname, "resolve.ts") });
+    });
+
+    test('"import" option', () => {
+        testTransform({ transform: path.join(__dirname, "import.ts"), import: "transformer" });
+    });
+
+    test("error if transformer could not be resolved", () => {
+        util.testModule``
+            .setOptions(optionsOfTransformer({ transform: path.join(__dirname, "error.ts") }))
+            .expectToHaveDiagnostics();
+    });
+});
+
+describe("factory types", () => {
+    for (const type of ["program", "config", "checker", "raw", "compilerOptions"] as const) {
+        test(type, () => {
+            const options = optionsOfTransformer({
+                transform: path.join(__dirname, "types.ts"),
+                type,
+                import: type,
+                value: true,
+            });
+
+            util.testFunction`
+                return false;
+            `
+                .setOptions(options)
+                .expectToEqual(true);
+        });
+    }
+});

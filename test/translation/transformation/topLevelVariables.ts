8df49ddd35480a54cbd2a9b025efde9e29860c1f
Perryvw <perry.wesel@gmail.com>
2020-01-09 22:22:15 +0100

    Added let variables in transformation test

--- test/translation/transformation/topLevelVariables.ts
+++ test/translation/transformation/topLevelVariables.ts
@@ -2,5 +2,9 @@ const obj = { value1: 1, value2: 2 };
 const value1 = obj.value1;
 const { value2 } = obj;
 
+let obj2 = { value3: 1, value4: 2 };
+let value3 = obj2.value3;
+let { value4 } = obj2;
+
 function fun1(): void {}
 const fun2 = () => {};

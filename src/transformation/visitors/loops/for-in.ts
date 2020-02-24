f98a33e2a994d6a26427086df148f045bc699ed7
hazzard993 <hazzard993@gmail.com>
2020-02-25 07:10:57 +1000

    Resolve exported identifiers in for-in loops (#829)

--- src/transformation/visitors/loops/for-in.ts
+++ src/transformation/visitors/loops/for-in.ts
@@ -4,6 +4,7 @@ import { FunctionVisitor } from "../../context";
 import { ForbiddenForIn, UnsupportedForInVariable } from "../../utils/errors";
 import { isArrayType } from "../../utils/typescript";
 import { transformIdentifier } from "../identifier";
+import { transformAssignment } from "../binary-expression/assignments";
 import { getVariableDeclarationBinding, transformLoopBody } from "./utils";
 
 export const transformForInStatement: FunctionVisitor<ts.ForInStatement> = (statement, context) => {
@@ -33,11 +34,14 @@ export const transformForInStatement: FunctionVisitor<ts.ForInStatement> = (stat
         // Iteration variable becomes ____key
         iterationVariable = lua.createIdentifier("____key");
         // Push variable = ____key to the start of the loop body to match TS scoping
-        const initializer = lua.createAssignmentStatement(
-            transformIdentifier(context, statement.initializer),
-            iterationVariable
+        const assignment = transformAssignment(
+            context,
+            statement.initializer,
+            iterationVariable,
+            statement.initializer
         );
-        body.statements.unshift(initializer);
+
+        body.statements.unshift(...assignment);
     } else {
         // This should never occur
         throw UnsupportedForInVariable(statement.initializer);

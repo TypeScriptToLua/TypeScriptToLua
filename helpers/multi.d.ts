09077c2282b0ac3e254ddb9a1e64e810c93da3eb
hazzard993 <hazzard993@gmail.com>
2020-02-25 15:05:25 +1000

    Add MultiReturn alias MR

--- helpers/multi.d.ts
+++ helpers/multi.d.ts
@@ -2,4 +2,6 @@ export type MultiReturn<T extends any[]> = T & {
     readonly " __multiBrand": unique symbol;
 };
 
+export { MultiReturn as MR };
+
 declare function multi<T extends any[]>(...values: T): MultiReturn<T>;

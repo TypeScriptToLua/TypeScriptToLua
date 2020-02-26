6bfd0287136e9a88a6ab19565b41c87e1a30cf0d
hazzard993 <hazzard993@gmail.com>
2020-02-26 10:55:01 +1000

    Remove MR alias

--- helpers/multi.d.ts
+++ helpers/multi.d.ts
@@ -2,6 +2,4 @@ export type MultiReturn<T extends any[]> = T & {
     readonly " __multiBrand": unique symbol;
 };
 
-export { MultiReturn as MR };
-
 export declare function multi<T extends any[]>(...values: T): MultiReturn<T>;

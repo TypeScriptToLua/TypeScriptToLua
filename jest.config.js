23c897209269ac6b2cfaffea5319f534ab5ccaeb
ark120202 <ark120202@gmail.com>
2020-02-29 10:22:03 +0000

    Revert "Use V8 code coverage"
    This reverts commit ac39a6bf846f3731f8734aa775766c0a047bcc68.

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ const isCI = require("is-ci");
 /** @type {Partial<import("@jest/types").Config.DefaultOptions>} */
 module.exports = {
     testMatch: ["**/test/**/*.spec.ts"],
-    coverageProvider: "v8",
     collectCoverageFrom: [
         "<rootDir>/src/**/*",
         "!<rootDir>/src/lualib/**/*",

65acb958889e9d36085c0f934acaeb543c6a4590
ark120202 <ark120202@gmail.com>
2020-03-11 16:15:58 +0000

    Add workaround for changed symbol ids

--- test/unit/functions/functions.spec.ts
+++ test/unit/functions/functions.spec.ts
@@ -487,7 +487,7 @@ test("Function rest parameter (referenced in property shorthand)", () => {
     `.expectToMatchJsResult();
 });
 
-test.skip("named function expression reference", () => {
+test("named function expression reference", () => {
     util.testFunction`
         const y = function x() {
             return { x: typeof x, y: typeof y };

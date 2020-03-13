b65f1fa602aec7a4e9d8bf88d7052db75c75789e
Chris Bajorin <chris@chrisbajorin.com>
2020-03-13 17:02:20 -0400

    cleanup

--- test/unit/classes/instanceof.spec.ts
+++ test/unit/classes/instanceof.spec.ts
@@ -49,22 +49,22 @@ test("null instanceof Class", () => {
     `.expectToMatchJsResult();
 });
 
-test("function instance of Class", () => {
+test("function instanceof Class", () => {
     util.testFunction`
         class myClass {}
-        const noop = () => {}
+        const noop = () => {};
         return (noop as any) instanceof myClass;
     `.expectToMatchJsResult();
 });
 
-test("boolean instance of Class", () => {
+test("boolean instanceof Class", () => {
     util.testFunction`
         class myClass {}
         return (false as any) instanceof myClass;
     `.expectToMatchJsResult();
 });
 
-test("number instance of Class", () => {
+test("number instanceof Class", () => {
     util.testFunction`
         class myClass {}
         return (5 as any) instanceof myClass;

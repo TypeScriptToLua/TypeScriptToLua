d2227fe57eb5d2b6ba283653fb8c21c2cdab7405
hazzard993 <hazzard993@gmail.com>
2020-03-29 12:32:01 +1000

    Add empty tuple to MultiReturn

--- helpers/multi.d.ts
+++ helpers/multi.d.ts
@@ -1,3 +1,3 @@
 declare function $multi<T extends any[]>(...values: T): MultiReturn<T>;
 
-type MultiReturn<T extends any[]> = T & { readonly " __multiBrand": unique symbol };
+type MultiReturn<T extends any[]> = T & [] & { readonly " __multiBrand": unique symbol };

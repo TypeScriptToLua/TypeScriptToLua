b2d856893a035105b55f8c8b76533c0b15bd8d68
Perryvw <perry.wesel@gmail.com>
2020-05-03 11:55:14 +0200

    Moved tests around

--- test/unit/statements.spec.ts
+++ test/unit/statements.spec.ts
@@ -1,6 +1,14 @@
 import * as util from "../util";
 import { unsupportedNodeKind } from "../../src/transformation/utils/diagnostics";
 
+test("Block statement", () => {
+    util.testFunction`
+        let a = 4;
+        { let a = 42; }
+        return a;
+    `.expectToMatchJsResult();
+});
+
 // https://github.com/TypeScriptToLua/TypeScriptToLua/issues/860
 test("Unsupported node adds diagnostic", () => {
     util.testModule`

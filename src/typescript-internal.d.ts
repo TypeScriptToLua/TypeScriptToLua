462727821805655bcb5f0d933cf2b94dfbbcdea4
ark120202 <ark120202@gmail.com>
2020-05-09 12:42:00 +0000

    Make typescript internal module augmentation a declaration file
    A workaround for typescript-eslint/typescript-eslint#1993.

---
+++ src/typescript-internal.d.ts
@@ -0,0 +1,23 @@
+export {};
+
+declare module "typescript" {
+    function createDiagnosticReporter(system: System, pretty?: boolean): DiagnosticReporter;
+    function createWatchStatusReporter(system: System, pretty?: boolean): WatchStatusReporter;
+
+    interface System {
+        setBlocking?(): void;
+    }
+
+    interface Statement {
+        jsDoc?: JSDoc[];
+    }
+
+    interface Program {
+        getCommonSourceDirectory(): string;
+    }
+
+    interface CompilerOptions {
+        configFile?: TsConfigSourceFile;
+        configFilePath?: string;
+    }
+}

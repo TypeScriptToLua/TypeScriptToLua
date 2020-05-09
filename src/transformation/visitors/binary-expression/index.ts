300d75a5f46b485331a1cd0c7c88d3ed2b80242f
Perryvw <perry.wesel@gmail.com>
2020-05-09 12:34:13 +0200

    Fixed type predicate, added tests for side effects

--- src/transformation/visitors/binary-expression/index.ts
+++ src/transformation/visitors/binary-expression/index.ts
@@ -5,7 +5,7 @@ import { AnnotationKind, getTypeAnnotations } from "../../utils/annotations";
 import { extensionInvalidInstanceOf, luaTableInvalidInstanceOf } from "../../utils/diagnostics";
 import { createImmediatelyInvokedFunctionExpression, wrapInToStringForConcat } from "../../utils/lua-ast";
 import { LuaLibFeature, transformLuaLibFunction } from "../../utils/lualib";
-import { isStandardLibraryType, isStringType } from "../../utils/typescript";
+import { isStandardLibraryType, isStringType, typeCanSatisfy } from "../../utils/typescript";
 import { transformTypeOfBinaryExpression } from "../typeof";
 import { transformAssignmentExpression, transformAssignmentStatement } from "./assignments";
 import { BitOperator, isBitOperator, transformBinaryBitOperation } from "./bit";
@@ -133,38 +133,7 @@ export const transformBinaryExpression: FunctionVisitor<ts.BinaryExpression> = (
         }
 
         case ts.SyntaxKind.QuestionQuestionToken: {
-            const lhsType = context.checker.getTypeAtLocation(node.left);
-            // Check if we can take a shortcut to 'lhs or rhs' if the left-hand side cannot be 'false'.
-            if (
-                lhsType.flags & (ts.TypeFlags.Any | ts.TypeFlags.Unknown | ts.TypeFlags.Boolean) ||
-                node.left.kind === ts.SyntaxKind.FalseKeyword
-            ) {
-                const lhsIdentifier = lua.createIdentifier("lhs");
-                const nilComparison = lua.createBinaryExpression(
-                    lua.cloneIdentifier(lhsIdentifier),
-                    lua.createNilLiteral(),
-                    lua.SyntaxKind.EqualityOperator
-                );
-                // if ____ == nil then return rhs else return ____ end
-                const ifStatement = lua.createIfStatement(
-                    nilComparison,
-                    lua.createBlock([lua.createReturnStatement([context.transformExpression(node.right)])]),
-                    lua.createBlock([lua.createReturnStatement([lua.cloneIdentifier(lhsIdentifier)])])
-                );
-                // (function(lhs') if lhs' == nil then return rhs else return lhs' end)(lhs)
-                return lua.createCallExpression(
-                    lua.createFunctionExpression(lua.createBlock([ifStatement]), [lhsIdentifier]),
-                    [context.transformExpression(node.left)]
-                );
-            } else {
-                // lhs or rhs
-                return lua.createBinaryExpression(
-                    context.transformExpression(node.left),
-                    context.transformExpression(node.right),
-                    lua.SyntaxKind.OrOperator,
-                    node
-                );
-            }
+            return transformNullishCoalescingExpression(context, node);
         }
 
         default:
@@ -204,5 +173,44 @@ export function transformBinaryExpressionStatement(
         ];
 
         return lua.createDoStatement(statements, expression);
+    }
+}
+
+function transformNullishCoalescingExpression(
+    context: TransformationContext,
+    node: ts.BinaryExpression
+): lua.Expression {
+    const lhsType = context.checker.getTypeAtLocation(node.left);
+
+    // Check if we can take a shortcut to 'lhs or rhs' if the left-hand side cannot be 'false'.
+    const typeCanBeFalse = (type: ts.Type) =>
+        (type.flags & (ts.TypeFlags.Any | ts.TypeFlags.Unknown | ts.TypeFlags.Boolean)) !== 0 ||
+        (type.flags & ts.TypeFlags.BooleanLiteral & ts.TypeFlags.PossiblyFalsy) !== 0;
+    if (typeCanSatisfy(context, lhsType, typeCanBeFalse)) {
+        // lhs can be false, transform to IIFE
+        const lhsIdentifier = lua.createIdentifier("____lhs");
+        const nilComparison = lua.createBinaryExpression(
+            lua.cloneIdentifier(lhsIdentifier),
+            lua.createNilLiteral(),
+            lua.SyntaxKind.EqualityOperator
+        );
+        // if ____ == nil then return rhs else return ____ end
+        const ifStatement = lua.createIfStatement(
+            nilComparison,
+            lua.createBlock([lua.createReturnStatement([context.transformExpression(node.right)])]),
+            lua.createBlock([lua.createReturnStatement([lua.cloneIdentifier(lhsIdentifier)])])
+        );
+        // (function(lhs') if lhs' == nil then return rhs else return lhs' end)(lhs)
+        return lua.createCallExpression(lua.createFunctionExpression(lua.createBlock([ifStatement]), [lhsIdentifier]), [
+            context.transformExpression(node.left),
+        ]);
+    } else {
+        // lhs or rhs
+        return lua.createBinaryExpression(
+            context.transformExpression(node.left),
+            context.transformExpression(node.right),
+            lua.SyntaxKind.OrOperator,
+            node
+        );
     }
 }
--- test/unit/nullishCoalescing.spec.ts
+++ test/unit/nullishCoalescing.spec.ts
@@ -1,11 +1,11 @@
 import * as util from "../util";
 
 test.each(["null", "undefined"])("nullish-coalesing operator returns rhs", nullishValue => {
-    util.testExpression`${nullishValue} ?? "Hello, World!"`.debug().expectToMatchJsResult();
+    util.testExpression`${nullishValue} ?? "Hello, World!"`.expectToMatchJsResult();
 });
 
 test.each([3, "foo", {}, [], true, false])("nullish-coalesing operator returns lhs", value => {
-    util.testExpression`${util.formatCode(value)} ?? "Hello, World!"`.debug().expectToMatchJsResult();
+    util.testExpression`${util.formatCode(value)} ?? "Hello, World!"`.expectToMatchJsResult();
 });
 
 test.each(["any", "unknown"])("nullish-coalesing operator with any/unknown type", type => {
@@ -21,8 +21,22 @@ test.each(["boolean | string", "number | false", "undefined | true"])(
         util.testFunction`
             const unknownType = false as ${unionType};
             return unknownType ?? "This should not be returned!";
-        `
-            .debug()
-            .expectToMatchJsResult();
+        `.expectToMatchJsResult();
     }
 );
+
+test("nullish-coalescing operator with side effect lhs", () => {
+    util.testFunction`
+        let i = 0;
+        const incI = () => ++i;
+        return [i, incI() ?? 3, i];
+    `.expectToMatchJsResult();
+});
+
+test("nullish-coalescing operator with side effect rhs", () => {
+    util.testFunction`
+        let i = 0;
+        const incI = () => ++i;
+        return [i, undefined ?? incI(), i];
+    `.expectToMatchJsResult();
+});

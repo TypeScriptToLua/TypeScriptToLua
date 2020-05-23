d1efec14ee4862d8cbe5f55dc22b3ec3de5f21be
ark120202 <ark120202@gmail.com>
2020-05-23 15:38:05 +0500

    Generator and iteration improvements (#872)
    * Generator and iteration improvements
    
    * Pass input vararg count to unpack
    
    * Fix .next() parameter not being passed
    
    * Methods are missing `FunctionExpressionFlags.Declaration`
    
    * Add changelog

--- src/lualib/Iterator.ts
+++ src/lualib/Iterator.ts
@@ -1,19 +1,32 @@
-function __TS__Iterator<T>(this: void, iterable: Iterable<T>): (this: void) => T {
-    if (iterable[Symbol.iterator]) {
+/** @tupleReturn */
+function __TS__IteratorGeneratorStep(this: GeneratorIterator): [true, any] | [] {
+    const co = this.____coroutine;
+
+    const [status, value] = coroutine.resume(co);
+    if (!status) throw value;
+
+    if (coroutine.status(co) === "dead") return [];
+    return [true, value];
+}
+
+/** @tupleReturn */
+function __TS__IteratorIteratorStep<T>(this: Iterator<T>): [true, T] | [] {
+    const result = this.next();
+    if (result.done) return [];
+    return [true, result.value];
+}
+
+/** @tupleReturn */
+function __TS__Iterator<T>(
+    this: void,
+    iterable: Iterable<T> | GeneratorIterator | readonly T[]
+): [(...args: any[]) => [any, T] | [], ...any[]] {
+    if ("____coroutine" in iterable) {
+        return [__TS__IteratorGeneratorStep, iterable];
+    } else if (iterable[Symbol.iterator]) {
         const iterator = iterable[Symbol.iterator]();
-        return () => {
-            const result = iterator.next();
-            if (!result.done) {
-                return result.value;
-            } else {
-                return undefined;
-            }
-        };
+        return [__TS__IteratorIteratorStep, iterator];
     } else {
-        let i = 0;
-        return () => {
-            i += 1;
-            return iterable[i];
-        };
+        return ipairs(iterable as readonly T[]);
     }
 }

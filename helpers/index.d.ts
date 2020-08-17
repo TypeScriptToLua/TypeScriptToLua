3c0f61e7a560d6a515609e50f434258111a17359
hazzard993 <hazzard993@gmail.com>
2020-08-17 11:07:50 +1000

    Merge helpers together into index.d.ts

--- helpers/index.d.ts
+++ helpers/index.d.ts
@@ -1,1 +1,2 @@
-import "./multi";
+declare function $multi<T extends any[]>(...values: T): MultiReturn<T>;
+declare type MultiReturn<T extends any[]> = T & { readonly " __multiBrand": unique symbol };
--- helpers/multi.d.ts
+++
@@ -1,2 +0,0 @@
-declare function $multi<T extends any[]>(...values: T): MultiReturn<T>;
-declare type MultiReturn<T extends any[]> = T & { readonly " __multiBrand": unique symbol };

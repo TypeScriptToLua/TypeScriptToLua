b72f47d865e7bec44f6578d311e7cd4aa6217c79
hazzard993 <hazzard993@gmail.com>
2020-09-19 13:00:57 +1000

    Move Decorators with descriptors tests to decorator tests

--- test/unit/classes/decorators.spec.ts
+++ test/unit/classes/decorators.spec.ts
@@ -183,3 +183,29 @@ test.each([["method(@decorator a) {}"], ["static method(@decorator a) {}"]])(
         `.expectToMatchJsResult();
     }
 );
+
+describe("Decorators /w descriptors", () => {
+    test.each([
+        ["return { writable: true }", "return { configurable: true }"],
+        ["desc.writable = true", "return { configurable: true }"],
+    ])("Combine decorators (%p + %p)", (decorateA, decorateB) => {
+        util.testFunction`
+            const A = (target, key, desc): any => { ${decorateA} };
+            const B = (target, key, desc): any => { ${decorateB} };
+            class Foo { @A @B static method() {} }
+            const { value, ...rest } = Object.getOwnPropertyDescriptor(Foo, "method");
+            return rest;
+        `.expectToMatchJsResult();
+    });
+
+    test.each(["return { value: true }", "desc.value = true"])(
+        "Use decorator to override method value",
+        overrideStatement => {
+            util.testFunction`
+                const decorator = (target, key, desc): any => { ${overrideStatement} };
+                class Foo { @decorator static method() {} }
+                return Foo.method;
+            `.expectToMatchJsResult();
+        }
+    );
+});

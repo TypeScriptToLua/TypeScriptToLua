fbb9f23e92f6ca28aab752b13ff87d7702fa40ca
ark120202 <ark120202@gmail.com>
2020-10-19 07:58:34 +0000

    Remove `baseUrl` tests
    It's not supposed to change runtime behavior by default, and should get
    the same treatment as `"paths"` (#591).

--- test/transpile/directories.spec.ts
+++ test/transpile/directories.spec.ts
@@ -2,31 +2,23 @@ import * as ts from "typescript";
 import * as tstl from "../../src";
 import { resolveFixture, transpileFilesResult } from "./run";
 
-interface DirectoryTestCase {
-    name: string;
-    options: tstl.CompilerOptions;
-}
+const projectRoot = resolveFixture("directories");
+test.each([{}, { outDir: "out" }, { rootDir: "src" }, { rootDir: "src", outDir: "out" }])(
+    "should be able to resolve (%p)",
+    tsconfigOptions => {
+        jest.spyOn(process, "cwd").mockReturnValue(projectRoot);
 
-test.each<DirectoryTestCase>([
-    { name: "basic", options: {} },
-    { name: "basic", options: { outDir: "out" } },
-    { name: "basic", options: { rootDir: "src" } },
-    { name: "basic", options: { rootDir: "src", outDir: "out" } },
-    { name: "baseurl", options: { baseUrl: "./src/lib", rootDir: ".", outDir: "./out" } },
-])("should be able to resolve (%p)", ({ name, options: compilerOptions }) => {
-    const projectPath = resolveFixture(`directories/${name}`);
-    jest.spyOn(process, "cwd").mockReturnValue(projectPath);
+        const config = {
+            compilerOptions: { ...tsconfigOptions, types: [], skipLibCheck: true },
+            tstl: { luaTarget: tstl.LuaTarget.LuaJIT, luaLibImport: tstl.LuaLibImportKind.Always },
+        };
 
-    const config = {
-        compilerOptions: { ...compilerOptions, types: [], skipLibCheck: true },
-        tstl: { luaTarget: tstl.LuaTarget.LuaJIT, luaLibImport: tstl.LuaLibImportKind.Always },
-    };
+        const { fileNames, options } = tstl.updateParsedConfigFile(
+            ts.parseJsonConfigFileContent(config, ts.sys, projectRoot)
+        );
 
-    const { fileNames, options } = tstl.updateParsedConfigFile(
-        ts.parseJsonConfigFileContent(config, ts.sys, projectPath)
-    );
-
-    const { diagnostics, emittedFiles } = transpileFilesResult(fileNames, options);
-    expect(diagnostics).not.toHaveDiagnostics();
-    expect(emittedFiles.map(f => f.name).sort()).toMatchSnapshot();
-});
+        const { diagnostics, emittedFiles } = transpileFilesResult(fileNames, options);
+        expect(diagnostics).not.toHaveDiagnostics();
+        expect(emittedFiles.map(f => f.name).sort()).toMatchSnapshot();
+    }
+);

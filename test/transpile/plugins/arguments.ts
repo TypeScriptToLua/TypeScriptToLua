83a99a16cf4eadf9bf5d0f7b8702a2920356b272
Justin Walsh <contact.me@thejustinwalsh.com>
2020-10-19 17:19:24 -0700

    Add test for argument passing in plugin config

---
+++ test/transpile/plugins/arguments.ts
@@ -0,0 +1,28 @@
+import * as ts from "typescript";
+import * as tstl from "../../../src";
+
+interface Options {
+    name: string;
+    option: boolean;
+}
+
+// eslint-disable-next-line import/no-default-export
+export default function plugin(options: Options): tstl.Plugin {
+    return {
+        visitors: {
+            [ts.SyntaxKind.ReturnStatement]: () =>
+                tstl.createReturnStatement([
+                    tstl.createTableExpression([
+                        tstl.createTableFieldExpression(
+                            tstl.createStringLiteral(options.name),
+                            tstl.createStringLiteral("name")
+                        ),
+                        tstl.createTableFieldExpression(
+                            tstl.createBooleanLiteral(options.option),
+                            tstl.createStringLiteral("option")
+                        ),
+                    ]),
+                ]),
+        },
+    };
+}

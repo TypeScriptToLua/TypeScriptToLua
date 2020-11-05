67870bfe73a950c5cf3168b9d2db97d3d292320d
Lorenz Junglas <lolleko@users.noreply.github.com>
2020-11-05 13:13:08 +0100

    Formatting & Adjusted script path

--- .github/scripts/create_benchmark_check.js
+++ .github/scripts/create_benchmark_check.js
@@ -1,17 +1,17 @@
-module.exports = ({github, context}) => {
+module.exports = ({ github, context }) => {
     const benchmarkInfoLua = JSON.parse(core.getInput("benchmark-info-lua", { required: true }));
     const benchmarkInfoJIT = JSON.parse(core.getInput("benchmark-info-jit", { required: true }));
-    
+
     const zlib = require("zlib");
     const buffer = Buffer.from(core.getInput("benchmark-info-lua", { required: true }));
     const compressed = zlib.deflateSync(buffer);
-    
+
     const summary =
         `[Open visualizer](https://typescripttolua.github.io/benchviz?d=${compressed.toString("base64")})\n` +
         `### Lua5.3\n${benchmarkInfoLua.comparison.summary}\n### LuaJIT\n${benchmarkInfoJIT.comparison.summary}`;
-    
+
     const text = `### Lua5.3\n${benchmarkInfoLua.comparison.text}\n### LuaJIT\n${benchmarkInfoJIT.comparison.text}`;
-    
+
     github.checks.create({
         owner: context.repo.owner,
         repo: context.repo.repo,
@@ -27,4 +27,4 @@ module.exports = ({github, context}) => {
     });
     console.log(summary);
     console.log(text);
-}
+};

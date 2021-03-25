fbc5d39a1da8f1e01417e889141a42ffe40a6738
Lorenz Junglas <lolleko@users.noreply.github.com>
2021-03-25 12:00:37 +0100

    Caught to more deprecated function calls

--- src/transpilation/transformers.ts
+++ src/transpilation/transformers.ts
@@ -45,11 +45,11 @@ export const noImplicitSelfTransformer: ts.TransformerFactory<ts.SourceFile | ts
     const transformSourceFile: ts.Transformer<ts.SourceFile> = node => {
         const empty = ts.factory.createNotEmittedStatement(undefined!);
         ts.addSyntheticLeadingComment(empty, ts.SyntaxKind.MultiLineCommentTrivia, "* @noSelfInFile ", true);
-        return ts.updateSourceFileNode(node, [empty, ...node.statements], node.isDeclarationFile);
+        return ts.factory.updateSourceFile(node, [empty, ...node.statements], node.isDeclarationFile);
     };
 
     return ts.isBundle(node)
-        ? ts.updateBundle(node, node.sourceFiles.map(transformSourceFile))
+        ? ts.factory.updateBundle(node, node.sourceFiles.map(transformSourceFile))
         : transformSourceFile(node);
 };
 

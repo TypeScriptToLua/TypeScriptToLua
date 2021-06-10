64aca6f9bf4df8e4b8361a950e0f74ebc8780c66
Perryvw <perry.wesel@gmail.com>
2021-06-10 17:26:36 +0200

    Fixed build issue

--- src/utils.ts
+++ src/utils.ts
@@ -50,7 +50,8 @@ export function formatPathToLuaPath(filePath: string): string {
 type NoInfer<T> = [T][T extends any ? 0 : never];
 
 export function getOrUpdate<K, V>(
-    map: Map<K, V> | (K extends Record<string, unknown> ? WeakMap<K, V> : never),
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    map: Map<K, V> | (K extends object ? WeakMap<K, V> : never),
     key: K,
     getDefaultValue: () => NoInfer<V>
 ): V {

4b8cb803564a6bae2c70f572e44ca17030e5f3c5
Perryvw <perry.wesel@gmail.com>
2021-06-20 12:57:19 +0200

    Also resolve tsx files

---
+++ test/transpile/module-resolution/project-with-tsx/dir/index.tsx
@@ -0,0 +1,3 @@
+export function indexf() {
+    return "hello from dir/index.tsx";
+}
---
+++ test/transpile/module-resolution/project-with-tsx/main.tsx
@@ -0,0 +1,5 @@
+import { f } from "./other";
+import { indexf } from "./dir";
+
+export const result = f();
+export const indexResult = indexf();

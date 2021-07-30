ec14feec9c2d9193a6abbc49a94002f866806f2c
Benjamin Ye <24237065+enjoydambience@users.noreply.github.com>
2021-07-30 08:57:39 -0700

    Add license for functions copied from sucrase

--- src/transformation/visitors/jsx/jsx.ts
+++ src/transformation/visitors/jsx/jsx.ts
@@ -29,7 +29,32 @@ function getJsxFragmentName(node: ts.Node, context: TransformationContext): lua.
     return getExpressionFromOption(option);
 }
 
-/* Implementations of jsx text processing modified from sucrase (https://github.com/alangpierce/sucrase). */
+/*
+The following 3 functions for jsx text processing modified from sucrase (https://github.com/alangpierce/sucrase), which
+is published with the MIT licence:
+
+The MIT License (MIT)
+
+Copyright (c) 2012-2018 various contributors (see AUTHORS)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 const HEX_NUMBER = /^[\da-fA-F]+$/;
 const DECIMAL_NUMBER = /^\d+$/;
@@ -139,6 +164,8 @@ function processEntity(text: string, indexAfterAmpersand: number): { entity: str
     }
     return { entity, newI: i };
 }
+
+// end functions copied from sucrase
 
 function processJsxText(jsxText: ts.JsxText): ts.StringLiteral | undefined {
     const text = formatJSXTextLiteral(jsxText.text);

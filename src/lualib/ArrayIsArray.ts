cca69493da8b6cf06d4b833a4ce71f19875b8d37
Perryvw <perry.wesel@gmail.com>
2021-08-17 21:49:22 +0200

    Fixed pull request link in ArrayIsArray lualib comment

--- src/lualib/ArrayIsArray.ts
+++ src/lualib/ArrayIsArray.ts
@@ -2,6 +2,6 @@ declare type NextEmptyCheck = (this: void, table: any, index: undefined) => unkn
 
 function __TS__ArrayIsArray(this: void, value: any): value is any[] {
     // Workaround to determine if value is an array or not (fails in case of objects without keys)
-    // See discussion in: https://github.com/TypeScriptToLua/TypeScriptToLua/pull/7
+    // See discussion in: https://github.com/TypeScriptToLua/TypeScriptToLua/pull/737
     return type(value) === "table" && (1 in value || (next as NextEmptyCheck)(value, undefined) === undefined);
 }

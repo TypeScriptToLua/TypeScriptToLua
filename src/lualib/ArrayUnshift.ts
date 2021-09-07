d45bf6d7451c389c891c5f4faf5c73c7fd5eb72c
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2021-09-07 16:58:40 -0700

    Optimize array.unshift

--- src/lualib/ArrayUnshift.ts
+++ src/lualib/ArrayUnshift.ts
@@ -1,6 +1,11 @@
-function __TS__ArrayUnshift<T>(this: void, arr: T[], ...items: T[]): number {
-    for (let i = items.length - 1; i >= 0; --i) {
-        table.insert(arr, 1, items[i]);
+function __TS__ArrayUnshift<T>(this: void, arr: T[], items: T[]): number {
+    const length = items.length;
+
+    for (const i of $range(arr.length, 1, -1)) {
+        arr[i + length - 1] = arr[i - 1];
+    }
+    for (const i of $range(1, length)) {
+        arr[i - 1] = items[i - 1];
     }
     return arr.length;
 }

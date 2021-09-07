ce108b456fb17b04a22621963ababadcb8c86f76
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2021-09-07 16:48:53 -0700

    Optimize array.flat

--- src/lualib/ArrayForEach.ts
+++ src/lualib/ArrayForEach.ts
@@ -3,7 +3,7 @@ function __TS__ArrayForEach<T>(
     arr: T[],
     callbackFn: (value: T, index?: number, array?: any[]) => any
 ): void {
-    for (let i = 0; i < arr.length; i++) {
-        callbackFn(arr[i], i, arr);
+    for (const i of $range(1, arr.length)) {
+        callbackFn(arr[i - 1], i - 1, arr);
     }
 }

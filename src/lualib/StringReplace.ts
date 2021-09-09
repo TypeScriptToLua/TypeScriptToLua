8b8223284d1ec13313e612cacadad44032c06a08
Benjamin Ye <24237065+GlassBricks@users.noreply.github.com>
2021-09-09 12:34:40 -0700

    Add `string.replaceAll` lualib and optimize/fix `string.replace` (#1129)
    * Optimize StringReplace
    
    * Add `string.replaceAll` lualib function
    
    This makes Node v15 (for ES2021 support) minimum required to run tests
    
    * Add tests for string.replace(All) function call arguments
    
    * Use node 16.9.0 for tests (for es2021 support)
    
    * Use node 15.14.0 for tests (for es2021 support)
    
    Node 16 with "vm" currently has a bug which prevents some js tests from compiling at all, somehow
    
    * Changes for PR

--- src/lualib/StringReplace.ts
+++ src/lualib/StringReplace.ts
@@ -2,21 +2,16 @@ function __TS__StringReplace(
     this: void,
     source: string,
     searchValue: string,
-    replaceValue: string | ((substring: string) => string)
+    replaceValue: string | ((match: string, offset: number, string: string) => string)
 ): string {
-    [searchValue] = string.gsub(searchValue, "[%%%(%)%.%+%-%*%?%[%^%$]", "%%%1");
-
-    if (typeof replaceValue === "string") {
-        [replaceValue] = string.gsub(replaceValue, "%%", "%%%%");
-        const [result] = string.gsub(source, searchValue, replaceValue, 1);
-        return result;
-    } else {
-        const [result] = string.gsub(
-            source,
-            searchValue,
-            match => (replaceValue as (substring: string) => string)(match),
-            1
-        );
-        return result;
+    const [startPos, endPos] = string.find(source, searchValue, undefined, true);
+    if (!startPos) {
+        return source;
     }
+    const sub = string.sub;
+    const before = sub(source, 1, startPos - 1);
+    const replacement =
+        typeof replaceValue === "string" ? replaceValue : replaceValue(searchValue, startPos - 1, source);
+    const after = sub(source, endPos + 1);
+    return before + replacement + after;
 }

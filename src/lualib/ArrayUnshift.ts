dbffcbc3d608bf94c89d6e7959c3a4d17c41836f
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2021-09-09 16:45:36 -0700

    Another optimization for array unshift

--- src/lualib/ArrayUnshift.ts
+++ src/lualib/ArrayUnshift.ts
@@ -1,5 +1,6 @@
 function __TS__ArrayUnshift<T>(this: void, arr: T[], items: T[]): number {
     const length = items.length;
+    if (length === 0) return arr.length;
 
     for (const i of $range(arr.length, 1, -1)) {
         arr[i + length - 1] = arr[i - 1];

8ce23505a1f72ece22ab2755a836c31623bd2d1a
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-02-02 15:38:23 -0800

    Add global assign test

--- test/transpile/lualib.spec.ts
+++ test/transpile/lualib.spec.ts
@@ -1,10 +1,31 @@
 import * as ts from "typescript";
 import { LuaLibFeature } from "../../src";
 import { readLuaLibFeature } from "../../src/LuaLib";
+import * as util from "../util";
 
 test.each(Object.entries(LuaLibFeature))("Lualib does not use ____exports (%p)", (_, feature) => {
     const lualibCode = readLuaLibFeature(feature, ts.sys);
 
     const exportsOccurrences = lualibCode.match(/____exports/g);
     expect(exportsOccurrences).toBeNull();
+});
+
+test("Lualib bundle does not assign globals", () => {
+    // language=TypeScript
+    util.testModule`
+        declare const _G: LuaTable;
+        declare const require: (this: void, module: string) => any;
+        const globalKeys = new LuaTable();
+        for (const [key] of _G) {
+            globalKeys[key] = true;
+        }
+        require("lualib_bundle");
+        for (const [key] of _G) {
+            if (!globalKeys[key]) {
+                error("Global was assigned: " + key);
+            }
+        }
+    `
+        .withLanguageExtensions()
+        .expectNoExecutionError();
 });

323bced577aa4068dad456b721402bb5fdf80cd0
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-02-07 09:20:49 -0800

    Remove now unnecessary imports from build-lualib.js

--- build-lualib.js
+++ build-lualib.js
@@ -1,9 +1,6 @@
 require("ts-node/register/transpile-only");
-const fs = require("fs");
 const path = require("path");
-const ts = require("typescript");
 const tstl = require("./src");
-const { loadInlineLualibFeatures } = require("./src/LuaLib");
 
 const configFileName = path.resolve(__dirname, "src/lualib/tsconfig.json");
 const { diagnostics } = tstl.transpileProject(configFileName);

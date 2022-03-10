c4796a607f7631bb93e36501448e177a7eb27159
Perryvw <perry.wesel@gmail.com>
2022-03-10 22:51:05 +0100

    fix prettier

--- test/transpile/plugins/beforeTransform.ts
+++ test/transpile/plugins/beforeTransform.ts
@@ -3,12 +3,12 @@ import * as tstl from "../../../src";
 
 const plugin: tstl.Plugin = {
     beforeTransform(program: ts.Program, options: tstl.CompilerOptions, emitHost: tstl.EmitHost) {
-        void(program);
-        void(emitHost);
+        void program;
+        void emitHost;
 
         // Modify settings
         options.outDir = "plugin/beforeTransform/outdir";
-    }
+    },
 };
 
 // eslint-disable-next-line import/no-default-export

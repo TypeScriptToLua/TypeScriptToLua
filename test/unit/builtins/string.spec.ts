8403960f387ed6de50b549dbe65bd5453db104cb
Perryvw <perry.wesel@gmail.com>
2022-03-14 20:36:22 +0100

    Added extra test covering more types

--- test/unit/builtins/string.spec.ts
+++ test/unit/builtins/string.spec.ts
@@ -379,3 +379,18 @@ test.each(['"foo"', "undefined"])("prototype call on nullable string (%p)", valu
         .setOptions({ strictNullChecks: true })
         .expectToMatchJsResult();
 });
+
+// Issue #1218: https://github.com/TypeScriptToLua/TypeScriptToLua/issues/1218
+test.each(["string | undefined", "string | null", "null | string", "null | undefined | string"])(
+    "prototype call on nullable string type (%p)",
+    type => {
+        util.testFunction`
+        function toUpper(str: ${type}) {
+            return str?.toUpperCase();
+        }
+        return toUpper("foo");
+    `
+            .setOptions({ strictNullChecks: true })
+            .expectToMatchJsResult();
+    }
+);

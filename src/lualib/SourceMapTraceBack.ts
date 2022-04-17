b67e3ba9cd728be999d9281c4b474443961a372a
pilaoda <793493083@qq.com>
2022-04-17 18:01:08 +0800

    fix: debug.traceback of loadstring get unknown file #1259

--- src/lualib/SourceMapTraceBack.ts
+++ src/lualib/SourceMapTraceBack.ts
@@ -43,8 +43,24 @@ export function __TS__SourceMapTraceBack(this: void, fileName: string, sourceMap
             let [result] = string.gsub(trace, "(%S+)%.lua:(%d+)", (file, line) =>
                 replacer(`${file}.lua`, `${file}.ts`, line)
             );
+
+            const stringReplacer = (file: string, line: string) => {
+                const fileSourceMap: SourceMap = globalThis.__TS__sourcemap[file];
+                if (fileSourceMap && fileSourceMap[line]) {
+                    const chunkName = string.match(file, '%[string "([^"]+)"%]')[0];
+                    const [sourceName] = string.gsub(chunkName, ".lua$", ".ts");
+                    const data = fileSourceMap[line];
+                    if (typeof data === "number") {
+                        return `${sourceName}:${data}`;
+                    }
+
+                    return `${data.file}:${data.line}`;
+                }
+
+                return `${file}:${line}`;
+            };
             [result] = string.gsub(result, '(%[string "[^"]+"%]):(%d+)', (file, line) =>
-                replacer(file, "unknown", line)
+                stringReplacer(file, line)
             );
 
             return result;

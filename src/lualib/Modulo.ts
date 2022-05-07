852febd4e2e13f1e0cc99b64671594908d86a75b
Ryan Young <ryan@youngryan.com>
2022-05-07 06:02:25 +0000

    style(lualib): use @noselfinfile for the modulo polyfill

--- src/lualib/Modulo.ts
+++ src/lualib/Modulo.ts
@@ -1,3 +1,5 @@
-export function __TS__Modulo(this: void, a: number, b: number): number {
+/** @noSelfInFile */
+
+export function __TS__Modulo(a: number, b: number): number {
     return a - Math.floor(a / b) * b;
 }

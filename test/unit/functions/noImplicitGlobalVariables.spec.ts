6892bdc8a82bf7d6ee0e062cdf1e5a51b9cf2dcd
James <5511220+Zamiell@users.noreply.github.com>
2022-05-06 21:57:08 -0400

    adding noImplicitGlobalVariables option

--- test/unit/functions/noImplicitGlobalVariables.spec.ts
+++ test/unit/functions/noImplicitGlobalVariables.spec.ts
@@ -1,5 +1,19 @@
 import * as util from "../../util";
 
+test("normal TSTL creates global variables", () => {
+    const fooBuilder = util.testModule`
+        function foo() {}
+        const bar = 123;
+    `
+        .expectToHaveNoDiagnostics();
+
+    const transpiledFile = fooBuilder.getLuaResult().transpiledFiles[0];
+    util.assert(transpiledFile !== undefined);
+    const { lua } =  transpiledFile;
+    util.assert(lua !== undefined);
+    expect(lua).not.toContain("local");
+});
+
 test("noImplicitGlobalVariables does not create any global variables", () => {
     const fooBuilder = util.testModule`
         function foo() {}

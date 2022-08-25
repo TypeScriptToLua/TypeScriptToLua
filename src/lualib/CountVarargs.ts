ccae8fd28feb3d79a5c236c737d96e5671b7979e
Ryan Young <ryan@youngryan.com>
2022-08-25 00:35:01 +0000

    fix(lualib): select() call was not using vararg optimization, breaking some spead cases

--- src/lualib/CountVarargs.ts
+++ src/lualib/CountVarargs.ts
@@ -3,7 +3,9 @@
 import { __TS__IsLua50 } from "./IsLua50";
 
 export function __TS__CountVarargs<T>(...args: T[]): number {
-    // In Lua 5.0, the arg table includes trailing nils. In all other
-    // versions, we must use select() to count trailing nils.
-    return __TS__IsLua50 ? args.length : select("#", ...args);
+    // select() is not available in Lua 5.0. In that version, the arg table
+    // includes trailing nils.
+    // It is important that the select() branch come first as we need vararg
+    // optimization for this call.
+    return !__TS__IsLua50 ? select("#", ...args) : args.length;
 }

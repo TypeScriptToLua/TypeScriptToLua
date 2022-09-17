a2219728eef64d346b17941697996ab8c9e27624
Ryan Young <ryan@youngryan.com>
2022-09-17 08:18:18 -0700

    fix(5.0): bad unpack() call in SparseArraySpread (#1347)

--- src/lualib/5.0/SparseArraySpread.ts
+++ src/lualib/5.0/SparseArraySpread.ts
@@ -1,5 +1,6 @@
 import { __TS__SparseArray } from "./SparseArray";
+import { __TS__Unpack } from "./Unpack";
 
 export function __TS__SparseArraySpread<T>(this: void, sparseArray: __TS__SparseArray<T>): LuaMultiReturn<T[]> {
-    return unpack(sparseArray, 1, sparseArray.sparseLength);
+    return __TS__Unpack(sparseArray, 1, sparseArray.sparseLength);
 }
---
+++ src/lualib/declarations/universal/unpack.d.ts
@@ -0,0 +1,4 @@
+/** @noSelfInFile */
+
+// Declare unpack for versions that have it instead of table.unpack
+declare const unpack: typeof table.unpack | undefined;

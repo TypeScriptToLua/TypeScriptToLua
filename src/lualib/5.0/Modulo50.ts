b3602f3fc7d636de3b2d99bf8f5bab04d0e350e2
Ryan Young <ryan@youngryan.com>
2022-11-25 02:43:24 +0000

    refactor(5.0): emulate modulo with native math.mod()

---
+++ src/lualib/5.0/Modulo50.ts
@@ -0,0 +1,3 @@
+/** @noSelfInFile */
+
+export const __TS__Modulo50 = math.mod;
---
+++ src/lualib/universal/Modulo50.ts
@@ -0,0 +1,5 @@
+/** @noSelfInFile */
+
+export function __TS__Modulo50(_a: number, _b: number) {
+    throw "Using a 5.0 shim for a non-5.0 target, what?";
+}

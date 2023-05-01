ac5c077df36b95085bdf437230dd47aeecc5d96c
Perryvw <perry.wesel@gmail.com>
2023-05-01 21:01:35 +0200

    Correctly merge tstl options from extended tsconfig

--- src/cli/tsconfig.ts
+++ src/cli/tsconfig.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 import * as ts from "typescript";
-import { CompilerOptions } from "../CompilerOptions";
+import { CompilerOptions, TypeScriptToLuaOptions } from "../CompilerOptions";
 import { normalizeSlashes } from "../utils";
 import * as cliDiagnostics from "./diagnostics";
 import { ParsedCommandLine, updateParsedConfigFile } from "./parse";
@@ -41,15 +41,67 @@ export function parseConfigFileWithSystem(
     commandLineOptions?: CompilerOptions,
     system = ts.sys
 ): ParsedCommandLine {
+    const configRootDir = path.dirname(configFileName);
     const parsedConfigFile = ts.parseJsonSourceFileConfigFileContent(
         ts.readJsonConfigFile(configFileName, system.readFile),
         system,
-        path.dirname(configFileName),
+        configRootDir,
         commandLineOptions,
         configFileName
     );
 
+    const cycleCache = new Set<string>();
+    const extendedTstlOptions = getExtendedTstlOptions(configFileName, configRootDir, cycleCache, system);
+
+    parsedConfigFile.raw.tstl = Object.assign(extendedTstlOptions, parsedConfigFile.raw.tstl ?? {});
+
     return updateParsedConfigFile(parsedConfigFile);
+}
+
+function getExtendedTstlOptions(
+    configFilePath: string,
+    configRootDir: string,
+    cycleCache: Set<string>,
+    system: ts.System
+): TypeScriptToLuaOptions {
+    const absolutePath = path.isAbsolute(configFilePath) ? configFilePath : path.resolve(configRootDir, configFilePath);
+    const newConfigRoot = path.dirname(absolutePath);
+
+    if (cycleCache.has(absolutePath)) {
+        return {};
+    }
+
+    cycleCache.add(absolutePath);
+    const fileContent = system.readFile(absolutePath);
+    const options = {};
+
+    if (fileContent) {
+        const parsedConfig = JSON.parse(fileContent) as {
+            extends?: string | string[];
+            tstl?: TypeScriptToLuaOptions;
+        };
+
+        if (parsedConfig.extends) {
+            if (Array.isArray(parsedConfig.extends)) {
+                for (const extendedConfigFile of parsedConfig.extends) {
+                    Object.assign(
+                        options,
+                        getExtendedTstlOptions(extendedConfigFile, newConfigRoot, cycleCache, system)
+                    );
+                }
+            } else {
+                Object.assign(options, getExtendedTstlOptions(parsedConfig.extends, newConfigRoot, cycleCache, system));
+            }
+        } else {
+            return parsedConfig.tstl ?? {};
+        }
+
+        if (parsedConfig.tstl) {
+            Object.assign(options, parsedConfig.tstl);
+        }
+    }
+
+    return options;
 }
 
 export function createConfigFileUpdater(

b1237739579792180318e53c1a3d44533abc5e4b
Perryvw <perry.wesel@gmail.com>
2023-06-04 17:16:19 +0200

    Add support for class static initialization blocks

--- test/unit/classes/classes.spec.ts
+++ test/unit/classes/classes.spec.ts
@@ -799,3 +799,22 @@ test.each(['(this["bar"])', '((((this["bar"]))))'])("methods in parentheses pass
         export const result = inst.foo();
     `.expectToMatchJsResult();
 });
+
+// https://github.com/TypeScriptToLua/TypeScriptToLua/issues/1447
+test("static initialization block (#1447)", () => {
+    util.testModule`
+        class A {
+            private static staticProperty1 = 3;
+            public static staticProperty2;
+            static {
+                this.staticProperty2 = this.staticProperty1 + 5;
+            }
+            public static staticProperty3;
+            static {
+                this.staticProperty3 = this.staticProperty1 + this.staticProperty2;
+            }
+        }
+        export const result1 = A.staticProperty2;
+        export const result2 = A.staticProperty3;
+    `.expectToMatchJsResult();
+});

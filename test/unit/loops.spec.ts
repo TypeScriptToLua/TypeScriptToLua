506b68b656a12c5ab2bece239a2054cd04ea26ce
Ryan Young <ryan@youngryan.com>
2023-10-16 20:03:47 +0000

    feat: support loop continue for 5.0, 5.1, and universal targets

--- test/unit/loops.spec.ts
+++ test/unit/loops.spec.ts
@@ -1,5 +1,5 @@
 import * as tstl from "../../src";
-import { forbiddenForIn, unsupportedForTarget } from "../../src/transformation/utils/diagnostics";
+import { forbiddenForIn } from "../../src/transformation/utils/diagnostics";
 import * as util from "../util";
 
 test("while", () => {
@@ -515,13 +515,16 @@ for (const testCase of [
     "for (const a in {}) { continue; }",
     "for (const a of []) { continue; }",
 ]) {
+    const expectContinueVariable: util.TapCallback = builder =>
+        expect(builder.getMainLuaCodeChunk()).toMatch("local __continue2");
+
     const expectContinueGotoLabel: util.TapCallback = builder =>
         expect(builder.getMainLuaCodeChunk()).toMatch("::__continue2::");
 
     util.testEachVersion(`loop continue (${testCase})`, () => util.testModule(testCase), {
-        [tstl.LuaTarget.Universal]: builder => builder.expectDiagnosticsToMatchSnapshot([unsupportedForTarget.code]),
-        [tstl.LuaTarget.Lua50]: builder => builder.expectDiagnosticsToMatchSnapshot([unsupportedForTarget.code]),
-        [tstl.LuaTarget.Lua51]: builder => builder.expectDiagnosticsToMatchSnapshot([unsupportedForTarget.code]),
+        [tstl.LuaTarget.Universal]: expectContinueVariable,
+        [tstl.LuaTarget.Lua50]: expectContinueVariable,
+        [tstl.LuaTarget.Lua51]: expectContinueVariable,
         [tstl.LuaTarget.Lua52]: expectContinueGotoLabel,
         [tstl.LuaTarget.Lua53]: expectContinueGotoLabel,
         [tstl.LuaTarget.Lua54]: expectContinueGotoLabel,

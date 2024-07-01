b4dc229e9e584fcf63bb3c36e84183f39112b255
Lorenz Junglas <lorenz.junglas@netlight.com>
2024-07-01 12:57:04 +0200

    Add lualib support for Math.trunc
    Also includes a small fix that makes `Math.sign` ECMA compliant
    
    Closes #1550

--- src/lualib/MathSign.ts
+++ src/lualib/MathSign.ts
@@ -1,9 +1,15 @@
+// https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-math.sign
+
+import { __TS__NumberIsNaN } from "./NumberIsNaN";
+
 export function __TS__MathSign(this: void, val: number) {
-    if (val > 0) {
-        return 1;
-    } else if (val < 0) {
+    if (__TS__NumberIsNaN(val) || val === 0) {
+        return val;
+    }
+
+    if (val < 0) {
         return -1;
     }
 
-    return 0;
+    return 1;
 }

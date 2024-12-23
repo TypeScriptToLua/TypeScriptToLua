8034f60b498d60575a3a20a6d0c9cd437669e422
Vurv <vurvdevelops@gmail.com>
2024-12-23 15:03:41 -0800

    Initial Luau implementation

--- src/transformation/visitors/break-continue.ts
+++ src/transformation/visitors/break-continue.ts
@@ -11,12 +11,17 @@ export const transformBreakStatement: FunctionVisitor<ts.BreakStatement> = (brea
 
 export const transformContinueStatement: FunctionVisitor<ts.ContinueStatement> = (statement, context) => {
     const scope = findScope(context, ScopeType.Loop);
-    const continuedWith =
-        context.luaTarget === LuaTarget.Universal ||
-        context.luaTarget === LuaTarget.Lua50 ||
-        context.luaTarget === LuaTarget.Lua51
-            ? LoopContinued.WithRepeatBreak
-            : LoopContinued.WithGoto;
+
+    const continuedWith = {
+        [LuaTarget.Universal]: LoopContinued.WithRepeatBreak,
+        [LuaTarget.Lua50]: LoopContinued.WithRepeatBreak,
+        [LuaTarget.Lua51]: LoopContinued.WithRepeatBreak,
+        [LuaTarget.Lua52]: LoopContinued.WithGoto,
+        [LuaTarget.Lua53]: LoopContinued.WithGoto,
+        [LuaTarget.Lua54]: LoopContinued.WithGoto,
+        [LuaTarget.LuaJIT]: LoopContinued.WithGoto,
+        [LuaTarget.Luau]: LoopContinued.WithContinue,
+    }[context.luaTarget];
 
     if (scope) {
         scope.loopContinued = continuedWith;
@@ -27,6 +32,9 @@ export const transformContinueStatement: FunctionVisitor<ts.ContinueStatement> =
     switch (continuedWith) {
         case LoopContinued.WithGoto:
             return lua.createGotoStatement(label, statement);
+
+        case LoopContinued.WithContinue:
+            return lua.createContinueStatement(statement);
 
         case LoopContinued.WithRepeatBreak:
             return [

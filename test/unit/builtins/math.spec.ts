986882be8a6c20c59b8eb501cad752d597370ec8
Vurv <vurvdevelops@gmail.com>
2024-12-27 13:27:33 -0800

    Add remaining Luau test suite cases
    Luau supports both math.atan & math.atan2 so having a certain result isn't important. Same goes for table.unpack and unpack.
    
    Had to pass an empty function for the continue case where luau wouldn't make a label at all since it has a proper continue statement.

--- test/unit/builtins/math.spec.ts
+++ test/unit/builtins/math.spec.ts
@@ -96,6 +96,7 @@ util.testEachVersion("Math.atan2", () => util.testExpression`Math.atan2(4, 5)`, 
     [tstl.LuaTarget.Lua52]: builder => builder.tap(expectMathAtan2),
     [tstl.LuaTarget.Lua53]: builder => builder.tap(expectMathAtan),
     [tstl.LuaTarget.Lua54]: builder => builder.tap(expectMathAtan2),
+    [tstl.LuaTarget.Luau]: builder => builder.tap(expectMathAtan2),
 });
 
 util.testEachVersion(
--- test/unit/loops.spec.ts
+++ test/unit/loops.spec.ts
@@ -553,6 +553,7 @@ for (const testCase of [
         [tstl.LuaTarget.Lua53]: expectContinueGotoLabel,
         [tstl.LuaTarget.Lua54]: expectContinueGotoLabel,
         [tstl.LuaTarget.LuaJIT]: expectContinueGotoLabel,
+        [tstl.LuaTarget.Luau]: () => {} // TODO: This is N/A.
     });
 }
 
